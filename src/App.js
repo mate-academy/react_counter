21f5c93d356290f7cb3104f1953a27aecbc5cdb4
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-12 14:02:53 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Не вызывайте здесь this.setState()!
+    this.state = {
+      counter: 0,
+    };
+  }
 
-    </div>
-  );
+  countAdder = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+
+  render() {
+    return (
+      <h1>
+        <button className="button" onClick={this.countAdder} />
+        <h2 className="count">{this.state.counter}</h2>
+      </h1>
+    );
+  }
 }
 
 export default App;

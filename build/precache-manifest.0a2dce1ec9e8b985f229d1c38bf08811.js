61d6835f3eff17170625444ee2024285d394e0dd
yevhen-sahach <antitop04@gmail.com>
2019-09-12 21:23:44 +0300

    first commit

---
+++ build/precache-manifest.0a2dce1ec9e8b985f229d1c38bf08811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2155e94095da94182ad3069e7672719",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9de62c31f9c8849408b6",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "./static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "9de62c31f9c8849408b6",
+    "url": "./static/js/main.6c2b77d0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+
+class Counter extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  onClickButton = () => {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1,
+    }));
+  }
+
+  render() {
+    const { onClickButton, state: { counter } } = this;
+    console.log(this);
+
+    return (
+      <>
+        <button type="button" onClick={onClickButton}>Click to add</button>
+        <div>{counter}</div>
+      </>
+    );
+  }
+}
 
 function App() {
   return (
     <div>
-
+      <Counter />
     </div>
   );
 }

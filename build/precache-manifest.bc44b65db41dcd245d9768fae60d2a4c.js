8dd0b2d02f4a2dabd298a433cd17a86889664c84
KseniiaMazan <ksusamazan@gmail.com>
2019-09-12 22:56:05 +0300

    solution

---
+++ build/precache-manifest.bc44b65db41dcd245d9768fae60d2a4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d933eb9df5014cf9904959299728a729",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b8fa45412ec7e2a22e75",
+    "url": "./static/css/main.fc7cc922.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "./static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "b8fa45412ec7e2a22e75",
+    "url": "./static/js/main.dfaa602b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ src/Component/Counter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Counter.css';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    }
+  }
+
+  onClickCounter = () => {
+    this.setState({
+      count: ++this.state.count,
+    });
+  }
+
+  render () {
+    return (
+      <>
+        <div className="counter">
+          Count: {this.state.count}
+        </div>
+        <button className="button" onClick={this.onClickCounter} type="button">
+          Click to increment!
+        </button>
+      </>
+    );
+  }
+}
+
+
+export default Counter;

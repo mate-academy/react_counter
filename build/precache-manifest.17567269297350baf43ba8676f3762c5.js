0647c7fbd009e764e29e484c0b202c5b2195580e
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-09-13 01:30:23 +0300

    solution

---
+++ build/precache-manifest.17567269297350baf43ba8676f3762c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e944147ceec0f2a01611cb9af21468",
+    "url": "./index.html"
+  },
+  {
+    "revision": "971a0caa9af53f7ec589",
+    "url": "./static/css/main.ada14ed5.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "./static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "971a0caa9af53f7ec589",
+    "url": "./static/js/main.0d43a8c2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-    </div>
-  );
+  onClick() {
+    this.setState({count: this.state.count + 1})
+  }
+
+  render() {
+    let { count } = this.state;
+    return (
+      <div className="main">
+        <button onClick={() => this.onClick()} type="button" className="btn btn-primary btn-lg">Count</button>
+        <div className="counter">
+          Count {count}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

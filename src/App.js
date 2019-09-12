597d7a6361c96d3e968775137913ecb27c37d911
unknown <shklyarigor2015@gmail.com>
2019-09-13 02:31:45 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import './App.css';
 
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      increment: 0,
+    }
+  }
+
+  onClickReaction = () => {
+    this.setState({
+      increment: ++this.state.increment,
+    });
+  }
+
+  render () {
+    return (
+      <div className="wrapper">
+        <button
+          className="btn"
+          onClick={this.onClickReaction}
+          type="button"
+        >
+          Click me
+        </button>
+        <div className="counter">
+          count: {this.state.increment}
+        </div>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div>
-
+      <Counter />
     </div>
   );
 }

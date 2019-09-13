7a7ff2e803cb33111386ff24ba9c6de2cf0891e8
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-13 14:17:04 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends React.Component {
   }
 
   countAdder = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
+    this.setState( prevState => ({
+      counter: prevState.counter + 1,
+    }));
   }
 
   render() {

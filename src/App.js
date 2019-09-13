00adad9fbf87abad6d98e18acc9b642092aa1909
vladyslav223 <vladyslav223@gmail.com>
2019-09-13 16:46:19 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/Counter';
 
-function App() {
-  return (
-    <Counter />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+   handleFunc = () => {
+    this.setState((prevState) => {
+
+      return {
+        counter: prevState.counter + 1,
+      }
+    });
+  }
+
+  render () {
+    return (
+      <>
+        <button id="btn" onClick={this.handleFunc} type="button">Click ME</button>
+        <div className="digital">count = {this.state.counter}</div>
+      </>
+    );
+  }
 }
 
 export default App;

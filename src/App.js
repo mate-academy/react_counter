58a41e94d5e47bb10a93a2b5156c9e8bf6383104
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-09-13 13:50:01 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ class App extends React.Component {
   };
 
   handleClick() {
-    this.setState({count: this.state.count + 1})
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
   }
 
   render() {
     let { count } = this.state;
     return (
       <div className="main">
-        <button onClick={() => this.handleClick()} type="button" className="btn btn-primary btn-lg">Count</button>
+        <button
+          onClick={() => this.handleClick()}
+          type="button"
+          className="btn btn-primary btn-lg">
+          Count
+        </button>
         <div className="counter">
           Count {count}
         </div>

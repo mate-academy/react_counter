b70856f6e34f16d61d5e5a9ec2df47eccfc6600b
Serhii-P <serhii.pyskun@gmail.com>
2019-09-13 16:55:58 +0300

    'Solution'

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './App.css';
 class App extends React.Component {
   state = {
-        counter: 0,
-      };
+    counter: 0,
+  };
 
-    handleClick = () => (
-      this.setState(prevState => ({
-      counter: prevState.counter + 1,
-    })) 
-    );
-  
+  handleClick = () => (
+    this.setState(prevState => ({
+    counter: prevState.counter + 1,
+    }))
+  );
+
   render() {
-     return (
-       <div>
-       <div className="resultValue">Count: {this.state.counter}</div>
-      <button className="btn" onClick={this.handleClick}>Click me!</button>
-      </div>
+    return (
+      <div>
+        <div className="resultValue">Count: {this.state.counter}</div>
+        <button className="btn" onClick={this.handleClick}>Click me!</button>
+    </div>
     );
   }
 }

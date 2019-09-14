f3e150002a972502a04c568f5593fdb9a0ba9f6d
Vlad Grytsenko <vladik.grytsenko2017@gmail.com>
2019-09-14 15:58:40 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  state = {
+      counter: 0,
+    };
 
-    </div>
-  );
+  increment = () => {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1,
+    }));
+  }
+
+  decrement = () => {
+    this.setState(prevState => (prevState.counter > 0
+      ? { counter: prevState.counter - 1 }
+      : { counter: 0 }));
+  }
+
+  reset = () => {
+    this.setState(() => ({
+      counter: 0,
+    }));
+  }
+
+  render() {
+    const { increment, decrement, reset } = this;
+
+    return (
+      <div className="wrapper">
+        <div className="counter">
+          Counter: {this.state.counter}
+        </div>
+        <button
+          className="button"
+          type="button"
+          onClick={increment}
+        >
+          Increase!
+        </button>
+        <button
+          className="button"
+          type="button"
+          onClick={decrement}
+        >
+          Decrease!
+        </button>
+        <button
+          className="button"
+          type="button"
+          onClick={reset}
+        >
+          Reset!
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;

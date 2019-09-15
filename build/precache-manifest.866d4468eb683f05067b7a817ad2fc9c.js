33fc58a69107c7dba919096cf333bbec360f881b
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-09-15 21:10:34 +0300

    Solution

---
+++ build/precache-manifest.866d4468eb683f05067b7a817ad2fc9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b397275955f8daa68f947050543f086",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c25587f7b6099b6c2456",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "./static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "c25587f7b6099b6c2456",
+    "url": "./static/js/main.38015a31.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
+// eslint-disable-next-line standard/object-curly-even-spacing
 
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // eslint-disable-next-line no-this-before-super
+    this.state = {
+      // eslint-disable-next-line react/no-unused-state
+      clicks: 0,
+      show: true
+    };
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  IncrementItem = () => {
+    this.setState(prevState => ({
+      clicks: prevState.clicks + 1,
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={() => this.IncrementItem()} type="button">Click me!</button>
+        <div>Count: {this.state.clicks}</div>
+      </>
+    );
+  }
 }
 
 export default App;

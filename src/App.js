af7c28ce0124482e469962460791af1cd7c4270d
igaryok <igor.sobko@gmail.com>
2019-09-17 11:57:23 +0300

    refactored after review

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 import Counter from './components/Counter/Counter';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Counter />
-    </div>
-  );
-}
+const App = () => <div><Counter /></div>;
 
 export default App;
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,27 +2,27 @@ import React from 'react';
 import './Counter.css';
 
 class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      counter: 0,
-    };
-    this.handleCounter = this.handleCounter.bind(this);
-  }
+  state = {
+    counter: 0,
+  };
 
-  handleCounter() {
-    this.setState(prevState => ({ counter: prevState.counter + 1 }));
-  }
+
+  handleCounter = () => this.setState(prevState => (
+    {
+      counter: prevState.counter + 1
+    }
+    ));
 
   render() {
     const { counter } = this.state;
+    const {  handleCounter } = this;
 
     return (
       <div className="place">
         <button
           type="button"
           className="btn btn-warning"
-          onClick={this.handleCounter}
+          onClick={handleCounter}
         >
           Add counter
         </button>

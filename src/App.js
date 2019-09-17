d8679e8b4ec1b5974edbdeb21d6130a34dc0c19f
Dmytro <voronok16@gmail.com>
2019-09-17 18:35:30 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,36 +2,44 @@ import React from 'react';
 import './App.css';
 
 class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-  }
+  state = {
+    count: 0,
+  };
 
   incrementCount = () => {
     this.setState(prevState => ({
       count: 1 + prevState.count,
     }));
-  }
+  };
 
   decrementCount = () => {
     this.setState(prevState => ({
       count: prevState.count - 1,
     }));
-  }
+  };
+
   resetCount = () => {
     this.setState({
       count: 0,
     });
-  }
+  };
+
   render() {
+    const {
+      count
+    } = this.state;
+    const {
+      incrementCount,
+      decrementCount,
+      resetCount
+    } = this;
+    
     return (
       <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.incrementCount} type="button">Increment</button>
-        <button onClick={this.decrementCount} type="button">decrement</button>
-        <button onClick={this.resetCount} type="button">reset</button>
+        <h1>{count}</h1>
+        <button onClick={incrementCount} type="button">Increment</button>
+        <button onClick={decrementCount} type="button">decrement</button>
+        <button onClick={resetCount} type="button">reset</button>
       </div>
     );
   }

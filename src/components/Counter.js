5664c86e4f51cc6b63fadd8e939664e1f2d05cb5
amserhii11 <amelin.serhii@gmail.com>
2019-09-17 14:50:18 +0300

    Fixed

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,23 +5,21 @@ class Counter extends React.Component {
     counter: 0,
   };
 
-  clickerCount = () => {
+  handleClick = () => {
     this.setState(prevState => ({
       counter: prevState.counter + 1,
     }));
   };
 
   render() {
+    const counter = this.state.counter;
+    const handleClick = this.handleClick;
     return (
       <>
-        <button
-          name="clicker"
-          className="ui button"
-          onClick={this.clickerCount}
-        >
+        <button name="clicker" className="ui button" onClick={handleClick}>
           Click me
         </button>
-        <div className="counter ui label">Counter: {this.state.counter}</div>
+        <div className="counter ui label">Counter: {counter}</div>
       </>
     );
   }

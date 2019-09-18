ade1228338d12c7914902c247eadce2754eb7540
Alina Kizilova <alinakzilova@gmail.com>
2019-09-18 12:31:30 +0300

    fixed

---
+++ build/precache-manifest.cfdd98b7c04307fd87a63f92c3b4e10f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e905c17b99428569ecaf61df32a4528c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e447ae200dd0153f486",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "./static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "0e447ae200dd0153f486",
+    "url": "./static/js/main.7897fa7f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);

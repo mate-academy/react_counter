8bee44fd95e9d9df3e473395d6913161f776c559
Serhii <gespenst.serge@yandex.ru>
2019-10-28 14:34:54 +0200

    sol

---
+++ build/precache-manifest.9000c3a46523a7eaef317cc679db66ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f057fcf3151c2a79b6154bb4e56ec5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "538344829d2c43c4b057",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "44eac7c1540a2ca2655c",
+    "url": "./static/js/2.7d73443f.chunk.js"
+  },
+  {
+    "revision": "538344829d2c43c4b057",
+    "url": "./static/js/main.1bdcdd00.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);

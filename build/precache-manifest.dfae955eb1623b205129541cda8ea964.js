2f6869a6e6b8618484236541b1cf60e890d6a246
nikakarelets <v.karelets@gmail.com>
2019-10-28 22:05:56 +0100

    Add build

---
+++ build/precache-manifest.dfae955eb1623b205129541cda8ea964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d46ac32d2fa63393f854e4780f7eaf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc708bea19ec4045e2b4",
+    "url": "./static/css/main.f5d7f296.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "./static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "fc708bea19ec4045e2b4",
+    "url": "./static/js/main.74d7eb1d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);

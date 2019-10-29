2462292e8788ceebeeb89b7e17bb77e03b1bde51
Dmitriymush <powder322@gmail.com>
2019-10-29 14:14:10 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Counter from "./components/Counter";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Counter />
+    </>
   );
 }
 

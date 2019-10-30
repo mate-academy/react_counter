225e409a7ea6ad54c0f2e19ad30a5604e76649c1
MMashivskij <mmashivskij@gmail.com>
2019-10-30 13:00:02 +0200

    Solutions

---
+++ build/precache-manifest.b2b1117d080d03f0f904dc3652fa201e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a435672c06fd196cdb663c7b68c25f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8d02b9fab895cfa1fbf",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "./static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "e8d02b9fab895cfa1fbf",
+    "url": "./static/js/main.b8b31007.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);

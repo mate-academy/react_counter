3baabf959e73d26d1bc6e3a25e079c7d7827b4b2
Viktoriia <jhunt@ukr.net>
2019-10-30 11:14:46 +0200

    solution

---
+++ build/precache-manifest.57734c4956d184aeecc62a6aa06cb76d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f32553e5c545d1a0c26986d3778762",
+    "url": "./index.html"
+  },
+  {
+    "revision": "457f2d90f2bb350ea134",
+    "url": "./static/css/main.a416d33c.chunk.css"
+  },
+  {
+    "revision": "dad80515a820c51d8df0",
+    "url": "./static/js/2.6ad4a00f.chunk.js"
+  },
+  {
+    "revision": "457f2d90f2bb350ea134",
+    "url": "./static/js/main.fd3f922a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    }
+
+    this.updateCount = this.updateCount.bind(this);
+  }
+
+  updateCount() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h3>Count: {this.state.count}</h3>
+        <button onClick={this.updateCount} className="ui primary button">
+          Click me
+        </button>
+      </>
+    );
+  }
+}
+
+export default Counter;

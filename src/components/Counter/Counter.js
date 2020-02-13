4a6a202fde5b9184c5de2fe8c732586a4ce7cb33
masha <mariafil2107@gmail.com>
2020-02-13 16:05:14 +0200

    counter

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './counter.css';
+
+export default class Counter extends Component {
+    state = {
+      value: 0,
+    }
+
+    handleIncrement = (e) => {
+      this.setState(prevState => ({
+        value: prevState.value + 1,
+      }));
+    }
+
+    render() {
+      const { value } = this.state;
+
+      return (
+        <div>
+          <h1>
+              Count:
+            {' '}
+            {value}
+          </h1>
+          <button type="button" onClick={this.handleIncrement}>
+                  Increment
+          </button>
+        </div>
+      );
+    }
+}

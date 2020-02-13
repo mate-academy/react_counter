c05c0474a4aaee2e062df46f8dae935ca2322242
masha <mariafil2107@gmail.com>
2020-02-13 16:44:02 +0200

    fixed indentation

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,30 +2,30 @@ import React, { Component } from 'react';
 import './counter.css';
 
 export default class Counter extends Component {
-    state = {
-      value: 0,
-    }
+  state = {
+    value: 0,
+  }
 
-    handleIncrement = (e) => {
-      this.setState(prevState => ({
-        value: prevState.value + 1,
-      }));
-    }
+  handleIncrement = (e) => {
+    this.setState(prevState => ({
+      value: prevState.value + 1,
+    }));
+  }
 
-    render() {
-      const { value } = this.state;
+  render() {
+    const { value } = this.state;
 
-      return (
-        <div>
-          <h1>
-              Count:
-            {' '}
-            {value}
-          </h1>
-          <button type="button" onClick={this.handleIncrement}>
+    return (
+      <div>
+        <h1>
+          Count:
+          {' '}
+          {value}
+        </h1>
+        <button type="button" onClick={this.handleIncrement}>
                   Increment
-          </button>
-        </div>
-      );
-    }
+        </button>
+      </div>
+    );
+  }
 }

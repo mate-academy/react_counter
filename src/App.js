46282f20d17e1cf3db943e09ba5397b301e1f960
Senya Volkov <formobileprima@gmail.com>
2020-02-14 13:58:53 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Count: 0</h1>
-);
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  increaseCounter = () => {
+    this.setState(prevState => (
+      { counter: prevState.counter + 1 }
+    ));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>
+          Count:
+          { this.state.counter }
+        </h1>
+
+        <button type="button" onClick={this.increaseCounter}>
+          increase counter
+        </button>
+      </>
+    );
+  }
+}

bd37b29c1fc80f662dbc5efb671f3bcca12f38d1
Nick <nikolyaoooyartchuk@gmail.com>
2020-04-24 02:45:52 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Count: 0</h1>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+
+    document.addEventListener('click', (event) => {
+      if (event.target.type !== 'button') {
+        return;
+      }
+
+      this.setState(prevState => ({
+        count: prevState.count + 1,
+      }));
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <button type="button" className="btn">More</button>
+        <h1>
+          Count:
+          {this.state.count}
+        </h1>
+      </>
+    );
+  }
+}
 
 export default App;

3c1225c392d4caf0948407eab25269940e9fcfb9
Oleksii Mykhailov <alem.coding@gmail.com>
2020-04-24 16:27:29 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
-const App = () => (
-  <h1>Count: 0</h1>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increase = () => {
+    this.setState(state => (
+      { count: state.count + 1 }
+    ));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>
+          {`Count: `}
+          {this.state.count}
+        </h1>
+        <button type="button" onClick={this.increase}>
+          Plus One
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;

de0535a01e86bb36a9bb85c114c98f5fcb0a064c
crash2bash <sachagurov@gmail.com>
2020-04-24 18:09:14 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Count: 0</h1>
-);
+class App extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  clickOnButton = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <h1 className="header">{`Count: ${this.state.count}`}</h1>
+        <button type="submit" onClick={this.clickOnButton} className="button">
+          Add one
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;

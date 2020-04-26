5d9881c9514e63b1daeb94755687d1ed5fc4fe8b
Andreas_Just <andreas.wq@gmail.com>
2020-04-26 05:22:01 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Count: 0</h1>
-);
+const App = () => {
+  const [count, SetCount] = useState(0);
+
+  return (
+    <div className="ui tag labels App">
+      <button
+        type="button"
+        onClick={() => SetCount(count + 1)}
+        className="ui big right labeled icon button"
+      >
+        <i className="up arrow icon" />
+        Increment
+      </button>
+      <h1 className="ui massive black label">
+        Count:
+        {count}
+      </h1>
+    </div>
+  );
+};
 
 export default App;

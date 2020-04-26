d03144eda7e4ede845c1a7f041d74fefbf2cae61
ShapovalDenys <denys.igorevich@gmail.com>
2020-04-26 15:11:52 +0300

    solution v0.1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,39 @@ import React from 'react';
 import './App.css';
 
 const App = () => (
-  <h1>Count: 0</h1>
+  <>
+    <h1 className="article">Please, press button!</h1>
+    <Counter />
+  </>
 );
+
+class Counter extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  click = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="counter">
+          Count:
+          {' '}
+          {this.state.count}
+        </h1>
+        <button
+          type="button"
+          className="button"
+          onClick={this.click}
+        >
+          Click
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;

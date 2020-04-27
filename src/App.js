f1def776e9e874dcca1cd774b7c65929e7c54940
Alex Machula <alexmachulamail@gmail.com>
2020-04-27 15:18:47 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Count: 0</h1>
-);
+class App extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  onClickButton = () => {
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>
+          Count:
+          {this.state.count}
+        </h1>
+        <button type="button" onClick={this.onClickButton}>
+          Click me
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;

80ae116f155dab7c4b7098aecee2f1e5be7867c9
Andrey Faychak <faychak333@gmail.com>
2020-04-27 17:12:38 +0300

    'oke'

--- src/App.js
+++ src/App.js
@@ -14,15 +14,13 @@ class App extends React.Component {
     this.setState(state => (
       { count: state.count + 1 }
     ));
-  }
+  };
 
   render() {
     return (
       <>
         <h1>
-          Count:
-          {' '}
-          {this.state.count}
+          {`Count: ${this.state.count}`}
         </h1>
 
         <button type="button" className="button" onClick={this.increase}>

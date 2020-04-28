4c7bbede172226ddd54a7553e3b914f5482bf26a
Eugene <evgnkuchma@gmail.com>
2020-04-28 04:12:22 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Count: 0</h1>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  additionClick = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  }
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <>
+        <h1>
+          Count:
+          {count}
+        </h1>
+        <button type="submit" onClick={this.additionClick}>
+          Click me!
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;

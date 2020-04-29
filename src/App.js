91d86e3e382e9e7ff1b28ae9fe30a6606f841480
Maksim Gryaznov <nineval@gmail.com>
2020-04-29 14:13:07 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Count: 0</h1>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  counterAdd = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>
+          Count:
+          {this.state.count}
+        </h1>
+        <button type="button" onClick={this.counterAdd}>Count</button>
+      </>
+    );
+  }
+}
 
 export default App;

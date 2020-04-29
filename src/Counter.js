12fc12fb6c195d0418f8f11dfc3d36ebbccbaa47
Andrii Badekha <badekha@gmail.com>
2020-04-29 13:55:22 +0300

    solution1

---
+++ src/Counter.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+
+class Counter extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  handleClick = () => {
+    this.setState(
+      prev => ({
+        count: prev.count + 1,
+      }),
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <h1>
+          {`Count: ${this.state.count}`}
+        </h1>
+        <button type="button" onClick={this.handleClick}>click</button>
+      </>
+    );
+  }
+}
+
+export default Counter;

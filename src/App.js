4364b3fcef2b56cda3ea0a47dd1dd4d6569e71a9
LimonovInsp <bis.limonov@gmail.com>
2020-06-25 12:13:50 +0300

    react counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <h1>Count: 0</h1>
-);
+class App extends React.Component {
+  state = {
+    counter: 0,
+  }
+
+    enlargement = () => {
+      this.setState(state => ({
+        counter: state.counter + 1,
+      }));
+    }
+
+    render() {
+      const { counter } = this.state;
+
+      return (
+        <>
+          <h1>
+            Count:
+            {counter}
+          </h1>
+          <button type="button" onClick={this.enlargement}>Increase</button>
+        </>
+      );
+    }
+}
 
 export default App;

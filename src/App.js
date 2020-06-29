8353329f2ff5ff3d2b967537f6c323f5151aa174
nazmeln <nazik100378@gmail.com>
2020-06-29 11:34:40 +0300

    removed constructor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0,
-    };
+  state = {
+    counter: 0,
   }
 
   increaseCounter = () => {

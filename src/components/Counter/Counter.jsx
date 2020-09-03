0be33ac50f2133ea84bf0515b57c05d5696b27fa
oivannikov <alex0502ivannikov@gmail.com>
2020-09-03 22:03:10 +0300

    solution

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const buttonsName = ['add 1', 'add 100', 'mix and devide 5'];
+
+export class Counter extends React.Component {
+    state = {
+      counter: 0,
+      buttons: buttonsName,
+    };
+  
+    checkButton = (buttonName) => {
+      if (buttonName === this.state.buttons[0]) {
+        this.addOne();
+      } else if (buttonName === this.state.buttons[1]) {
+        this.add100();
+      } else if (buttonName === this.state.buttons[2]) {
+        this.increase();
+      }
+    }
+
+    addOne = () => {
+      this.setState({counter: this.state.counter += 1});
+    };
+  
+    add100 = () => {
+      this.setState({counter: this.state.counter += 100});
+    };
+  
+    increase = () => {
+      this.addOne();
+      if (this.state.counter % 5 === 0) {
+        this.add100();
+      }
+    };
+  
+    render() {
+      return (
+        <div className="section">
+          <h1 className="section__header-title">Counter: {this.state.counter}</h1>
+          {
+            buttonsName.map((buttonName, index) => (
+              <button
+                className={classNames("section__button", {
+                  'section__button-clicked':buttonName === buttonsName[index]
+                })}
+                onClick={() => {
+                  this.checkButton(buttonName);
+                }}
+              >
+                {buttonName}
+              </button>
+            ))
+          }
+        </div>
+      )
+  }
+}
+

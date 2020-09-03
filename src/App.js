35f148870b327df372a3cef9577fee691592acda
uni-8 <julia.zhol.a@gmail.com>
2020-09-03 16:31:39 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,15 +2,53 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    counter: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => this.setState(state => ({ counter: state.counter + 1 }));
 
-  increase = () => {};
+  add100 = () => this.setState(state => ({ counter: state.counter + 100 }));
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.counter % 5) {
+      return;
+    }
+
+    this.add100();
+    document.body.style.background = `#${Math.random().toString().slice(2, 8)}`;
+  };
 
   render() {
+    const { counter } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>{`Count: ${counter}`}</h1>
+
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          Add 1
+        </button>
+
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          Magic
+        </button>
+      </>
     );
   }
 }

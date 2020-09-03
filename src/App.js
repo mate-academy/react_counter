db43ee10ac41bbf3ccad90cb2c9223413ae7a83e
Roman <romikkus@gmail.com>
2020-09-03 19:29:12 +0300

    Removed unnecessary check

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   increase = () => {
     this.addOne();
 
-    if ((this.state.counter + 1) % 5 === 0 && this.state.counter !== 0) {
+    if ((this.state.counter + 1) % 5 === 0) {
       this.add100();
     }
   };

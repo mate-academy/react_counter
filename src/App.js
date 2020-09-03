f3078ddf06f08823a629932f97313250dcd87d16
Elena-brv <leena.brv@gmail.com>
2020-09-03 21:24:44 +0300

    Counter

--- src/App.js
+++ src/App.js
@@ -2,15 +2,50 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+    this.setState((state) => {
+      if (state.count % 5 === 0) {
+        this.add100();
+      }
+    });
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {count}
+        </h1>
+        <button type="button" onClick={this.addOne}>
+          Add 1
+        </button>
+        <button type="button" onClick={this.add100}>
+          Add 100
+        </button>
+        <button type="button" onClick={this.increase}>
+          Surprise
+        </button>
+      </>
     );
   }
 }

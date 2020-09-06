05550df66116fc1c590ac4dc28a29a91189b184e
DmJSCourse <dimonua@gmail.com>
2020-09-06 14:47:51 +0300

    done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,53 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  add1 = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  }
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({ count: state.count + 100 }));
+  };
+
+  increase = () => {
+    this.add1();
+    this.setState((state) => {
+      if (state.count % 5 === 0) {
+        this.add100();
+      }
+    });
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {this.state.count}
+        </h1>
+        <button
+          type="button"
+          onClick={this.add1}
+        >
+          Add 1
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          Add 1 (and 100 if factor of 5)
+        </button>
+      </>
     );
   }
 }

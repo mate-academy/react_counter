20df3c014a30de06599dbc63a7311c36165d5e76
Serhii Antonyshyn <serginio4422@gmail.com>
2020-09-24 13:51:01 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -2,15 +2,51 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState((state) => {
+      if (state.count % 5 === 0) {
+        this.add100();
+        this.addOne();
+      }
+    });
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1>
+          Count:
+          {' '}
+          {count}
+        </h1>
+        <button type="button" onClick={this.addOne}>
+          Click here to add 1!
+        </button>
+        <button type="button" onClick={this.add100}>
+          Click here to add 100!
+        </button>
+        <button type="button" onClick={this.increase}>
+          Click here to increase if count is divisible by 5!
+        </button>
+      </div>
     );
   }
 }

4de717e17c0de3b15220dbd95243d10f3a1acfae
OleksandrMykoliuk <69759907+OleksandrMykoliuk@users.noreply.github.com>
2020-10-25 19:21:16 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -2,15 +2,62 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div>
+        <h1>
+          {
+            `Count: ${count}`
+          }
+        </h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+          className="btn btn-outline-success"
+        >
+          Add one!
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+          className="btn btn-outline-warning"
+        >
+          Add hundred!
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+          className="btn btn-outline-danger"
+        >
+          Add one or hundred!
+        </button>
+      </div>
     );
   }
 }

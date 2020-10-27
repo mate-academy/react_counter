0ab9b9d5a5c054520a15f1b4e903a0d1e115d713
Anastasia Chervona <av.chervona@gmail.com>
2020-10-27 16:08:01 +0200

    Refactoring made

--- src/App.js
+++ src/App.js
@@ -3,10 +3,47 @@ import './App.scss';
 
 import { Counter } from './components/Counter';
 
-const App = () => (
-  <div className="app">
-    <Counter />
-  </div>
-);
+class App extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  addOne = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    if (count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div className="app">
+        <Counter
+          count={count}
+          addOne={this.addOne}
+          add100={this.add100}
+          increase={this.increase}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,66 +1,40 @@
 import React from 'react';
 import './Counter.scss';
 
-export class Counter extends React.Component {
-  state = {
-    count: 0,
-  }
+import { CounterShape } from '../shapes/CounterShape';
 
-  addOne = () => {
-    this.setState(prevState => ({
-      count: prevState.count + 1,
-    }));
-  };
+export const Counter = ({ count, addOne, add100, increase }) => (
+  <div className="counter">
+    <h1 className="counter__header">
+      {`Count: ${count}`}
+    </h1>
 
-  add100 = () => {
-    this.setState(prevState => ({
-      count: prevState.count + 100,
-    }));
-  };
+    <div className="counter__buttons">
+      <button
+        type="button"
+        onClick={addOne}
+        className="counter__button"
+      >
+        Add 1
+      </button>
 
-  increase = () => {
-    const { count } = this.state;
+      <button
+        type="button"
+        onClick={add100}
+        className="counter__button"
+      >
+        Add 100
+      </button>
 
-    if (count % 5 === 0) {
-      this.add100();
-    }
+      <button
+        type="button"
+        onClick={increase}
+        className="counter__button"
+      >
+        Increase
+      </button>
+    </div>
+  </div>
+);
 
-    this.addOne();
-  };
-
-  render() {
-    return (
-      <div className="counter">
-        <h1 className="counter__header">
-          {`Count: ${this.state.count}`}
-        </h1>
-
-        <div className="counter__buttons">
-          <button
-            type="button"
-            onClick={this.addOne}
-            className="counter__button"
-          >
-            Add 1
-          </button>
-
-          <button
-            type="button"
-            onClick={this.add100}
-            className="counter__button"
-          >
-            Add 100
-          </button>
-
-          <button
-            type="button"
-            onClick={this.increase}
-            className="counter__button"
-          >
-            Increase
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+Counter.propTypes = CounterShape;

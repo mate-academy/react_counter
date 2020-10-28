00f1f0678870f10d62ce154aba81b217429e5c97
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-28 17:39:02 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
 import './App.scss';
 
+import { Button } from 'semantic-ui-react';
+
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    counter: 0,
+  };
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(prevState => ({ counter: prevState.counter + 1 }));
+  }
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(prevState => ({ counter: prevState.counter + 100 }));
+  }
+
+  increase = () => {
+    if ((this.state.counter % 5) === 0 && this.state.counter !== 0) {
+      this.add100();
+      this.addOne();
+    } else {
+      this.addOne();
+    }
+  }
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="app">
+        <h1 className="app__title">
+          Count:
+          {this.state.counter}
+        </h1>
+        <Button.Group>
+          <Button
+            type="button"
+            className="app__button"
+            onClick={this.addOne}
+          >
+            Add One
+          </Button>
+          <Button
+            type="button"
+            className="app__button"
+            onClick={this.add100}
+          >
+            Add 100
+          </Button>
+          <Button
+            primary
+            type="button"
+            className="app__button"
+            onClick={this.increase}
+          >
+            Show Time
+          </Button>
+        </Button.Group>
+      </div>
     );
   }
 }

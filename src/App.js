90a196f1665ac6d3ec622ae5cebd823d6cf5e3c4
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-28 12:40:29 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -2,15 +2,65 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  };
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="app">
+        <h1 className="app__header">
+          Count:
+          {' '}
+          {count}
+        </h1>
+
+        <div className="app__buttons">
+          <button
+            className="app__button"
+            type="button"
+            onClick={this.addOne}
+          >
+            add 1
+          </button>
+          <button
+            className="app__button"
+            type="button"
+            onClick={this.add100}
+          >
+            add 100
+          </button>
+          <button
+            className="app__button"
+            type="button"
+            onClick={this.increase}
+          >
+            increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

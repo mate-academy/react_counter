752e58d1837b61379a40efe7758beb68f0c6612a
Your Name <mogolgogol.90@gmail.com>
2021-01-16 16:12:53 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
+import { Counter } from './components/Counter';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
-
-  add100 = () => {};
-
-  increase = () => {};
-
-  render() {
-    return (
-      <h1>Count: 0</h1>
-    );
-  }
+function App() {
+  return (
+    <main>
+      <Counter />
+    </main>
+  );
 }
 
 export default App;

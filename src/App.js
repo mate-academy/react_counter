dd29a7b73be3bb8b2b0ea7b8466f417f74424476
Layrovell <marina95prodan@gmail.com>
2021-01-18 16:22:19 +0200

    develop

--- src/App.js
+++ src/App.js
@@ -2,15 +2,64 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  };
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (!(this.state.count % 5)) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="center-block">
+          <h1>
+            Count:
+            {' '}
+            {this.state.count}
+          </h1>
+          <div className="btn-group" role="group" aria-label="Basic example">
+            <button
+              onClick={this.addOne}
+              type="button"
+              className="btn btn-info"
+            >
+              add 1
+            </button>
+            <button
+              onClick={this.add100}
+              type="button"
+              className="btn btn-success"
+            >
+              increase
+            </button>
+            <button
+              onClick={this.increase}
+              type="button"
+              className="btn btn-warning"
+            >
+              add 100
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }

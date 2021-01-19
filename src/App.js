d931f979c91c3a90a1edb34ac64b648f802329cb
Olesia <olesia.tk@gmail.com>
2021-01-19 15:13:13 +0200

    Solition

--- src/App.js
+++ src/App.js
@@ -2,15 +2,43 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({ count: state.count + 100 }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.setState(state => ({ count: state.count + 100 }));
+    }
+
+    this.setState(state => ({ count: state.count + 1 }));
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div>
+        <h1>
+          Count:
+          {this.state.count}
+        </h1>
+        <button type="button" onClick={this.addOne}>
+          addOne
+        </button>
+        <button type="button" onClick={this.add100}>
+          add100
+        </button>
+        <button type="button" onClick={this.increase}>
+          Increase
+        </button>
+      </div>
     );
   }
 }

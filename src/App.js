ec417cbe351eafae4f24883b42743fe4d255e421
Alex <alexmogwaiii@gmail.com>
2021-01-19 18:04:29 +0200

    Counter was created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import { Counter } from './conponents/Counter';
+
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  }
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState(state => (
+      state.count % 5 === 0
+        ? (this.add100(), this.addOne())
+        : this.addOne()
+    ));
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <Counter
+        addOne={this.addOne}
+        add100={this.add100}
+        increase={this.increase}
+        count={this.state.count}
+      />
     );
   }
 }

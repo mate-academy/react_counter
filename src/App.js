bfe5a1760a04d2a2e82fc1e1c8adf22ca7cc63cf
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-18 12:27:02 +0200

    counter solution

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.scss';
+import Counter from './components/Counter';
 
 class App extends React.Component {
-  addOne = () => {};
-
-  add100 = () => {};
-
-  increase = () => {};
-
   render() {
     return (
-      <h1>Count: 0</h1>
+      <Counter
+        add100={this.add100}
+        increase={this.increase}
+      />
     );
   }
 }

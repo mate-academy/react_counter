2a6f97e694f49aa6649c3356270a4f87f8f711f8
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-23 15:09:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,64 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  clearCount = () => {
+    this.setState(prevState => ({
+      count: 0,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1 className="header">
+          Count:
+          {' '}
+          {
+            count
+          }
+        </h1>
+        <button type="button" onClick={this.addOne}>
+          Add one
+        </button>
+        {' '}
+        <button type="button" onClick={this.add100}>
+          Add 100
+        </button>
+        {' '}
+        <button type="button" onClick={this.increase}>
+          Increase
+        </button>
+        {' '}
+        <button type="button" onClick={this.clearCount}>
+          Clear
+        </button>
+        {' '}
+      </>
     );
   }
 }

ab263e92ce188f7a0c3bb0b8b93f43ee440104d2
Smihotur Roman <smikhotur@gmail.com>
2021-03-29 19:01:13 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -2,15 +2,75 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState(prevState => ({
+      count: (prevState.count % 5 !== 0)
+        ? prevState.count + 1
+        : prevState.count + 101,
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      count: 0,
+    });
+  }
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <section className="counter">
+        <h1 className="counter__title">
+          {`Count: ${count}`}
+        </h1>
+        <section className="counter__block">
+          <button
+            className="counter__btn"
+            type="button"
+            onClick={this.addOne}
+          >
+            Add one
+          </button>
+          <button
+            className="counter__btn"
+            type="button"
+            onClick={this.add100}
+          >
+            Add 100
+          </button>
+          <button
+            className="counter__btn"
+            type="button"
+            onClick={this.increase}
+          >
+            Increase
+          </button>
+          <button
+            className="counter__btn"
+            type="button"
+            onClick={this.reset}
+          >
+            Reset
+          </button>
+        </section>
+      </section>
     );
   }
 }

96a64fd9f5c75f0c8579d0053d4ba76ecbf2dd65
Bohdan Prokopov <b.prokopov96@gmail.com>
2021-04-06 12:40:25 +0300

    solved

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+export class App extends React.Component {
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    if (count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="container">
+        <h1 className="text-center m-5">
+          Count:
+          {count}
+        </h1>
+        <div className="btn-group d-flex justify-content-center">
+          <button
+            type="button"
+            className="btn rounded btn-danger m-2"
+            onClick={() => this.addOne()}
+          >
+            Add 1
+          </button>
+          <button
+            type="button"
+            className="btn rounded btn-success m-2"
+            onClick={() => this.add100()}
+          >
+            Add 100
+          </button>
+          <button
+            type="button"
+            className="btn rounded btn-warning m-2"
+            onClick={() => this.increase()}
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     );
   }
 }
-
-export default App;

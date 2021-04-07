fb43f059efc2beeedded79939102b38801780e0f
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2021-04-07 16:14:38 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,16 +1,66 @@
 import React from 'react';
-import './App.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(prev => ({
+      count: prev.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(prev => ({
+      count: prev.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div>
+        <h1 className="ml-5">
+          Count:
+          {this.state.count}
+        </h1>
+        <div>
+          <Button
+            type="button"
+            onClick={this.addOne}
+            variant="outline-primary"
+            className="ml-5"
+          >
+            Add 1
+          </Button>
+          <Button
+            type="button"
+            onClick={this.add100}
+            variant="outline-secondary"
+            className="ml-3"
+          >
+            Add 100
+          </Button>
+          <Button
+            type="button"
+            onClick={this.increase}
+            variant="outline-success"
+            className="ml-3"
+          >
+            increase
+          </Button>
+        </div>
+      </div>
     );
   }
 }

4cf596ddcfcb2bc3254ed021b7d7161c92b4b8cd
Oleg Hluhk <glug.oleg@gmail.com>
2021-04-07 13:01:24 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
 import React from 'react';
 import './App.scss';
+import 'bulma';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    if (count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
+
+  reset = () => {
+    this.setState({
+      count: 0,
+    });
+  }
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="app">
+        <h1 className="title">
+          {`Count: ${count}`}
+        </h1>
+        <div className="button-box">
+          <button
+            className="button is-outlined is-primary"
+            type="button"
+            onClick={this.addOne}
+          >
+            Add one
+          </button>
+          <button
+            className="button is-outlined is-primary"
+            type="button"
+            onClick={this.add100}
+          >
+            Add 100
+          </button>
+          <button
+            className="button is-outlined is-primary"
+            type="button"
+            onClick={this.increase}
+          >
+            Increase
+          </button>
+          <button
+            className="button is-outlined is-primary"
+            type="button"
+            onClick={this.reset}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     );
   }
 }

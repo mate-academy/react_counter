ad6efc54b229cb02e653ee480c4b6a3e0f42ea76
Nazar Matsevych <nazar6192111@gmail.com>
2021-05-22 23:34:52 +0300

    solution

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './Counter.scss';
+
+export class Counter extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  addOne = () => {
+    this.setState(({ count }) => ({
+      count: count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(({ count }) => ({
+      count: count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState(({ count }) => {
+      this.addOne();
+      if (count % 5 === 0) {
+        this.add100();
+      }
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div className="wrapper">
+        <h1 className="title">
+          Count:
+          {' '}
+          {count}
+        </h1>
+        <button
+          className="title__button"
+          type="button"
+          onClick={this.addOne}
+        >
+          +1
+        </button>
+        <button
+          className="title__button"
+          type="button"
+          onClick={this.add100}
+        >
+          +3
+        </button>
+        <button
+          className="title__button"
+          type="button"
+          onClick={this.increase}
+        >
+          increase
+        </button>
+      </div>
+    );
+  }
+}

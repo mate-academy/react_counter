84dbc2c5bc75db989d5b149ac8424c00669098b9
Igor Lisogor <illisogori@gmail.com>
2021-05-25 14:56:25 +0300

    Counter moved to components

---
+++ src/components/Counter.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export class Counter extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  addOne = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({ count: state.count + 100 }));
+  };
+
+  increase = () => {
+    this.addOne();
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <h1>
+          Count:
+          {this.state.count}
+        </h1>
+        <button type="button" onClick={() => this.addOne()}>
+          Add One
+        </button>
+        <button type="button" onClick={() => this.add100()}>
+          Add 100
+        </button>
+        <button type="button" onClick={() => this.increase()}>
+          Increase
+        </button>
+      </>
+    );
+  }
+}

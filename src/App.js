8452646a28d82b7e932a71519445b4b06f7cb18f
Yurii Lisovenko <yriyliso@gmail.com>
2021-05-25 15:45:35 +0300

    created counter

--- src/App.js
+++ src/App.js
@@ -2,15 +2,58 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {this.state.count}
+        </h1>
+
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          Add One
+        </button>
+
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+      </>
     );
   }
 }

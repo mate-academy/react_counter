13d1651d0f2ad298e2e3a8a2b853a9476f9147d5
Maria Yasevina <maria.yasevina@gmail.com>
2021-05-25 14:54:14 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,61 +1,10 @@
 import React from 'react';
+import { Counter } from './components/Counter';
+
 import './App.scss';
 
-class App extends React.Component {
-  state = {
-    count: 0,
-  }
-
-  addOne = () => {
-    this.setState(prevState => ({
-      count: prevState.count + 1,
-    }));
-  };
-
-  add100 = () => {
-    this.setState(prevState => ({
-      count: prevState.count + 100,
-    }));
-  };
-
-  increase = () => {
-    this.setState(({ count }) => (
-      (count % 5 === 0) && count
-        ? this.add100()
-        : this.addOne()
-    ));
-  };
-
-  render() {
-    const { count } = this.state;
-
-    return (
-      <>
-        <h1>
-          {`Count: ${count}`}
-        </h1>
-
-        <button
-          type="button"
-          onClick={this.addOne}
-        >
-          Adds 1
-        </button>
-        <button
-          type="button"
-          onClick={this.add100}
-        >
-          Adds 100
-        </button>
-        <button
-          type="button"
-          onClick={this.increase}
-        >
-          Increase
-        </button>
-      </>
-    );
-  }
-}
+const App = () => (
+  <Counter />
+);
 
 export default App;

c39a5f87a13e669b5934c8b3e847b3aced23968f
Taras Kozak <kozaktarasofficial@gmail.com>
2021-05-25 10:34:43 +0300

    v1.00

--- src/App.js
+++ src/App.js
@@ -2,15 +2,72 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0
+        && this.state.count !== 0) {
+      this.setState(state => (
+        {
+          count: state.count + 100,
+        }));
+    }
+
+    this.setState(state => (
+      {
+        count: state.count + 1,
+      }));
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1 className="App__title">
+          {`Count: ${count}`}
+        </h1>
+
+        <div className="App__button">
+          <button
+            type="button"
+            onClick={this.addOne}
+            className="App__button-addsOne"
+          >
+            adds 1
+          </button>
+
+          <button
+            type="button"
+            onClick={this.add100}
+            className="App__button-addsHundred"
+          >
+            adds 100
+          </button>
+
+          <button
+            type="button"
+            onClick={this.increase}
+            className="App__button-increase"
+          >
+            increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

e927119f0594af8086878a55f9916e68503ad216
Vasyl Habalevych <vasylhab@gmail.com>
2021-05-25 14:38:31 +0200

    Reac counter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export class Counter extends React.Component {
+  state = {
+    counter: 0,
+  }
+
+  addOne = () => {
+    this.setState(state => ({
+      counter: state.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({
+      counter: state.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+    if (this.state.counter % 5 === 0) {
+      this.add100();
+    }
+  };
+
+  render() {
+    const { counter } = this.state;
+
+    return (
+      <>
+        <h1>{`Count: ${counter}`}</h1>
+        <button type="button" onClick={this.addOne}>
+          Add 1
+        </button>
+
+        <button type="button" onClick={this.add100}>
+          Add 100
+        </button>
+
+        <button type="button" onClick={this.increase}>
+          Increase
+        </button>
+      </>
+    );
+  }
+}

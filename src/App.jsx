41059dbd26930f2f7d09ec00d1f25456db371504
Bishko <yurik322412@gmail.com>
2021-07-17 20:09:40 +0300

    counter react

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1>
+          {'Count: '}
+          {this.state.count}
+        </h1>
+        <button type="button" onClick={this.addOne}>Add 1</button>
+        <button type="button" onClick={this.add100}>Add 100</button>
+        <button type="button" onClick={this.increase}>Increase</button>
+      </div>
+    );
+  }
+}
+
+export default App;

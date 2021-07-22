e765743d9ecdabdd6d15e63d410ee2e7ca0b28ad
Tolik Bilokrylov <enkorn1715@gmail.com>
2021-07-22 18:39:13 +0300

    done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,113 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {`${count}`}
+        </h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          Add 1
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+      </>
     );
   }
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+
+// import './App.scss';
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [click, setClick] = useState(0);
+
+//   function increase() {
+//     if (click % 5 === 0) {
+//       setCount(count + 101);
+//     } else {
+//       setCount(count + 1);
+//     }
+//   }
+
+//   return (
+//     <>
+//       <h1>
+//         Count:
+//         {count}
+//       </h1>
+//       <button
+//         type="button"
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >
+//         Add 1
+//       </button>
+//       <button
+//         type="button"
+//         onClick={() => {
+//           setCount(count + 100);
+//         }}
+//       >
+//         Add 100
+//       </button>
+//       <button
+//         type="button"
+//         onClick={() => {
+//           setClick(click + 1);
+//           increase();
+//         }}
+//       >
+//         Increase
+//       </button>
+//     </>
+//   );
+// }
+
+// export default App;

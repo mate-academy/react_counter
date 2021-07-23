7be84a12152e9fabb3c29e718d02cb16d9548278
Tolik Bilokrylov <enkorn1715@gmail.com>
2021-07-23 14:28:06 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Counter } from './Caunter';
 import './App.scss';
 
 class App extends React.Component {
@@ -27,88 +28,15 @@ class App extends React.Component {
   };
 
   render() {
-    const { count } = this.state;
-
     return (
-      <>
-        <h1>
-          Count:
-          {`${count}`}
-        </h1>
-        <button
-          type="button"
-          onClick={this.addOne}
-        >
-          Add 1
-        </button>
-        <button
-          type="button"
-          onClick={this.add100}
-        >
-          Add 100
-        </button>
-        <button
-          type="button"
-          onClick={this.increase}
-        >
-          Increase
-        </button>
-      </>
+      <Counter
+        count={this.state.count}
+        addOne={this.addOne}
+        add100={this.add100}
+        increase={this.increase}
+      />
     );
   }
 }
 
 export default App;
-
-// import React, { useState } from 'react';
-
-// import './App.scss';
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [click, setClick] = useState(0);
-
-//   function increase() {
-//     if (click % 5 === 0) {
-//       setCount(count + 101);
-//     } else {
-//       setCount(count + 1);
-//     }
-//   }
-
-//   return (
-//     <>
-//       <h1>
-//         Count:
-//         {count}
-//       </h1>
-//       <button
-//         type="button"
-//         onClick={() => {
-//           setCount(count + 1);
-//         }}
-//       >
-//         Add 1
-//       </button>
-//       <button
-//         type="button"
-//         onClick={() => {
-//           setCount(count + 100);
-//         }}
-//       >
-//         Add 100
-//       </button>
-//       <button
-//         type="button"
-//         onClick={() => {
-//           setClick(click + 1);
-//           increase();
-//         }}
-//       >
-//         Increase
-//       </button>
-//     </>
-//   );
-// }
-
-// export default App;

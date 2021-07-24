0e6fba0d56f6dfeb7ed7ba34646ef1b9aeefc1c2
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-24 18:30:11 +0300

    task is completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+import { Counter } from './components/Counter';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    this.addOne();
+
+    if (count % 5 === 0 && count !== 0) {
+      this.add100();
+    }
+  };
+
+  reset = () => {
+    this.setState({ count: 0 });
+  }
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="container">
+        <Counter
+          count={count}
+          addOne={this.addOne}
+          add100={this.add100}
+          increase={this.increase}
+          reset={this.reset}
+        />
+      </div>
     );
   }
 }

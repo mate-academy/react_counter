14355e9c7ab0fe554900c93ce7f7e8390c3c37a5
Vitaliy Martynyuk <vitaliymartiniuk16@gmail.com>
2021-07-26 12:42:01 +0300

    separate component file

--- src/App.js
+++ src/App.js
@@ -1,62 +1,7 @@
 import React from 'react';
 import './App.scss';
+import Counter from './Counter';
 
-class App extends React.Component {
-  state = {
-    counter: 0,
-  };
-
-  addOne = () => {
-    this.setState(prevState => ({
-      counter: prevState.counter + 1,
-    }));
-  };
-
-  add100 = () => {
-    this.setState(prevState => ({
-      counter: prevState.counter + 100,
-    }));
-  };
-
-  increase = () => {
-    this.addOne();
-    if (!(this.state.counter % 5) && this.state.counter) {
-      this.add100();
-    }
-  };
-
-  reset = () => {
-    this.setState(prevState => ({
-      counter: 0,
-    }));
-  };
-
-  render() {
-    return (
-      <>
-        <div>
-          <h1>
-            Count:
-            {this.state.counter}
-          </h1>
-          <button type="button" onClick={this.addOne}>
-            Add 1
-          </button>
-          <button type="button" onClick={this.add100}>
-            Add 100
-          </button>
-          <button type="button" onClick={this.increase}>
-            Add ?
-          </button>
-        </div>
-        <div>
-          <button type="button" onClick={this.reset}>
-            Reset
-          </button>
-        </div>
-      </>
-    );
-  }
-}
+const App = () => <Counter />;
 
 export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+class Counter extends React.PureComponent {
+    state = {
+        counter: 0,
+    };
+
+    addOne = () => {
+        this.setState(prevState => ({
+            counter: prevState.counter + 1,
+        }));
+    };
+
+    add100 = () => {
+        this.setState(prevState => ({
+            counter: prevState.counter + 100,
+        }));
+    };
+
+    increase = () => {
+        this.addOne();
+            if (!(this.state.counter % 5) && this.state.counter) {
+                this.add100();
+            }
+    }
+
+    reset = () => {
+        this.setState({
+            counter: 0
+        })
+    };
+
+    render () {
+        return (
+            <>
+              <div>
+                <h1>
+                  Count:
+                  {this.state.counter}
+                </h1>
+                <button type="button" onClick={this.addOne}>
+                  Add 1
+                </button>
+                <button type="button" onClick={this.add100}>
+                  Add 100
+                </button>
+                <button type="button" onClick={this.increase}>
+                  Add ?
+                </button>
+              </div>
+              <div>
+                <button type="button" onClick={this.reset}>
+                  Reset
+                </button>
+              </div>
+            </>
+        )
+    }
+};
+
+export default Counter;

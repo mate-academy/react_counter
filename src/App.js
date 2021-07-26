eb23c672824c0da94712eae4c0751e19c391f3cc
Pavlo Starynskyi <starinskiy@gmail.com>
2021-07-26 17:48:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import './App.scss';
+import { Counter } from './components';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };;
+
+  increase = () => {
+    this.setState(this.addOne);
+    if (this.state.count % 5 === 0) {
+      this.setState(this.add100);
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <Counter
+        count={this.state.count}
+        addOne={this.addOne}
+        add100={this.add100}
+        increase={this.increase}
+      />
     );
   }
 }

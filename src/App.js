04a4c637f2912f1444f753e4a0030b90a7313e30
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-07-26 09:51:58 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -2,15 +2,62 @@ import React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(
+      state => ({ count: state.count + 1 }),
+    );
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(
+      state => ({ count: state.count + 100 }),
+    );
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="box">
+        <h1>
+          {`Count: ${count}`}
+        </h1>
+        <div className="buttons are-medium is-centered">
+          <button
+            className="button is-warning"
+            onClick={this.addOne}
+            type="button"
+          >
+            add 1
+          </button>
+          <button
+            className="button is-primary"
+            onClick={this.add100}
+            type="button"
+          >
+            add 100
+          </button>
+          <button
+            className="button is-info"
+            onClick={this.increase}
+            type="button"
+          >
+            increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

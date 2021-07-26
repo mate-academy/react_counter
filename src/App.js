0cc5391abef7d8a9757e32e220e3e697c0180321
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-07-26 18:01:51 +0300

    Solution, styles in progress

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import './App.scss';
+import { Counter } from './components';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    count: 0,
+  }
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  }
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <Counter
+        count={this.state.count}
+        add100={this.add100}
+        addOne={this.addOne}
+        increase={this.increase}
+      />
     );
   }
 }

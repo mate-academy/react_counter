7a66f796a5240410d8e11cd75a3fdbf3e3041af9
Kostrica <romiorw@ukr.net>
2021-08-22 12:22:38 +0300

    Solutio

---
+++ src/components/Counter.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { State } from '../types/State';
+import { Props } from '../types/Props';
+
+class Counter extends React.Component<Props, State> {
+  state = {
+    count: 0,
+  };
+
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div>
+        <h1>
+          Count:
+          {count}
+        </h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          addOne
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          add100
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          increase
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;

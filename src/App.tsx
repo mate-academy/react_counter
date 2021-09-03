88a5ecb3993105d7d0a1727f91db724796e3c224
Andrii Serhovetc <serhovetc88@gmail.com>
2021-09-03 13:47:36 +0300

    react-counter done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,68 @@
+/* eslint-disable no-console */
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number;
+};
+class App extends React.Component<{}, State> {
+  state: State = {
+    counter: 0,
+  };
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState((state) => ({
+      counter: state.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.counter % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { counter } = this.state;
+    const { addOne, add100, increase } = this;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="container">
+        <div className="counter">
+          <h1>{`Count: ${counter}`}</h1>
+          <div className="counter-btn-wrap">
+            <button
+              className="counter__btn"
+              type="button"
+              onClick={addOne}
+            >
+              add 1
+            </button>
+            <button
+              className="counter__btn"
+              type="button"
+              onClick={add100}
+            >
+              add 100
+            </button>
+            <button
+              className="counter__btn"
+              type="button"
+              onClick={increase}
+            >
+              increase
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }

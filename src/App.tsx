7a2c816ca234ecc6a2c5348298547af54038ec04
Ihor Nahnybida <igor653279@gmail.com>
2021-09-05 19:08:00 +0300

    counter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+interface State {
+  counter: number;
+}
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    const { counter } = this.state;
+
+    this.addOne();
+
+    if (counter % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { counter } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          {`Count: ${counter}`}
+        </h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          Add one
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          Add one hundred
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+      </>
     );
   }
 }

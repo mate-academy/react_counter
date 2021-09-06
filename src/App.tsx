54b3baf9cad971d7b5d9e396c9011059233f04f5
Taras Holiuk <taras.holiuk@gmail.com"dsveagj 3rm>
2021-09-06 17:42:52 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,81 @@
 import React from 'react';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends React.Component {
-  addOne = () => {};
+interface State {
+  count: number;
+}
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => {
+      return {
+        count: state.count + 1,
+      };
+    });
+  };
+
+  add100 = () => {
+    this.setState((state) => {
+      return {
+        count: state.count + 100,
+      };
+    });
+  };
+
+  increase = () => {
+    this.setState((state) => {
+      this.addOne();
+
+      return state.count % 5 === 0
+        ? this.add100()
+        : null;
+    });
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <div className="container">
+          <h1 className="title">
+            Count:
+            {' '}
+            {count}
+          </h1>
+
+          <div className="row">
+            <button
+              type="button"
+              className="button col p-2 p-xl-3"
+              onClick={this.addOne}
+            >
+              Add One
+            </button>
+
+            <button
+              type="button"
+              className="button col p-2 p-xl-3"
+              onClick={this.add100}
+            >
+              Add 100
+            </button>
+
+            <button
+              type="button"
+              className="button col p-2 p-xl-3"
+              onClick={this.increase}
+            >
+              Increase
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }

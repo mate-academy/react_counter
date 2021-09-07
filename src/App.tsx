1a2f06794e2240a8cf7d90e834a8b2df72cf924e
Ula Dutchak <ulana.dutchak@gmail.com>
2021-09-07 17:32:08 +0300

    add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,15 @@
 import React from 'react';
 import './App.scss';
+import { Counter } from './components/Counter';
 
-type State = {
-  count: number;
-};
-
-class App extends React.Component<{}, State> {
-  state = {
-    count: 0,
-  };
-
-  addOne = () => {
-    this.setState((prevCount) => ({
-      count: prevCount.count + 1,
-    }));
-  };
-
-  add100 = () => {
-    this.setState((prevCount) => ({
-      count: prevCount.count + 100,
-    }));
-  };
-
-  increase = () => {
-    this.addOne();
-
-    if (this.state.count % 5 === 0) {
-      this.add100();
-    }
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App__wrapper">
-          <h1 className="App__title">
-            {`Count: ${this.state.count}`}
-          </h1>
-          <div className="App__buttons">
-            <button
-              className="App__button addOne"
-              type="button"
-              onClick={this.addOne}
-            >
-              add one
-            </button>
-            <button
-              className="App__button add100"
-              type="button"
-              onClick={this.add100}
-            >
-              add 100
-            </button>
-            <button
-              className="App__button increase"
-              type="button"
-              onClick={this.increase}
-            >
-              increase
-            </button>
-          </div>
-        </div>
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="App__wrapper">
+        <Counter />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;

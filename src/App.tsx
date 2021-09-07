40589d48bb40b3ef0656c398cd1673b1d354def6
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-09-07 16:55:28 +0300

    pilol

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,73 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (!(this.state.count % 5)) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <main className="main">
+        <h1>
+          {'Count: '}
+          {count}
+        </h1>
+        <div
+          className="btn-group"
+          role="group"
+          aria-label="Basic mixed styles example"
+        >
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={this.addOne}
+          >
+            Add 1
+          </button>
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={this.add100}
+          >
+            Add 100
+          </button>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={this.increase}
+          >
+            Some magic
+          </button>
+        </div>
+      </main>
     );
   }
 }

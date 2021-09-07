b15532339d2427ce11528af803c72e7e0429da8b
Sasha Lifashkin <alifashkin@outlook.com>
2021-09-07 17:47:02 +0300

    comments del in code

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,6 @@ import React from 'react';
 import './App.scss';
 import { Counter } from './components/Counter';
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Counter />
-//       </div>
-//     );
-//   }
-// }
-
 const App: React.FC = () => {
   return (
     <div className="App">

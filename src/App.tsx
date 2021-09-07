3b262c75114aae49d63c0eb5c5a33fb5cc74e815
karolina-nadDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD <westlinka20@gmail.com>
2021-09-07 22:34:24 +0300

    done2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,61 @@
 import React from 'react';
 import { Counter } from './Counter';
 
-const App: React.FC<{}> = () => (<Counter />);
+type State = {
+  count: number;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
+
+  addOne = () => {
+    this.setState((currentState) => ({ count: currentState.count + 1 }));
+  };
+
+  add100 = () => {
+    this.setState((currentState) => ({ count: currentState.count + 100 }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <Counter count={this.state.count} />
+        <div className="app__buttons">
+          <button
+            type="button"
+            className="app__button"
+            onClick={this.addOne}
+          >
+            addOne
+          </button>
+          <button
+            type="button"
+            className="app__button"
+            onClick={this.add100}
+          >
+            add100
+          </button>
+          <button
+            type="button"
+            className="app__button"
+            onClick={this.increase}
+          >
+            increase
+          </button>
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,64 +1,17 @@
 import React from 'react';
 import './App.scss';
 
-type State = {
+type Props = {
   count: number;
 };
 
-export class Counter extends React.Component<{}, State> {
-  state: State = {
-    count: 0,
-  };
-
-  addOne = () => {
-    this.setState((state) => ({ count: state.count + 1 }));
-  };
-
-  add100 = () => {
-    this.setState((state) => ({ count: state.count + 100 }));
-  };
-
-  increase = () => {
-    this.setState((state) => {
-      this.addOne();
-
-      if (state.count % 5 === 0) {
-        this.add100();
-      }
-    });
-  };
-
-  render() {
-    return (
-      <div className="app">
-        <h1 className="app__title">
-          Count:
-          {this.state.count}
-        </h1>
-        <div className="app__buttons">
-          <button
-            type="button"
-            className="app__button"
-            onClick={this.addOne}
-          >
-            addOne
-          </button>
-          <button
-            type="button"
-            className="app__button"
-            onClick={this.add100}
-          >
-            add100
-          </button>
-          <button
-            type="button"
-            className="app__button"
-            onClick={this.increase}
-          >
-            increase
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+export const Counter: React.FC<Props> = ({ count }) => {
+  return (
+    <div className="app">
+      <h1 className="app__title">
+        Count:
+        {count}
+      </h1>
+    </div>
+  );
+};

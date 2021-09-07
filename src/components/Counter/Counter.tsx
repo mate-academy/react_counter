9a09b90694b9d75abf6e42a7ec5c9880380b587a
Dima Tkachenko <DimaTkachenko3@gmail.com>
2021-09-07 19:25:40 +0300

    renamed is done

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -12,14 +12,14 @@ export class Counter extends React.Component<{}, State> {
   };
 
   addOne = () => {
-    this.setState((state) => ({
-      count: state.count + 1,
+    this.setState((currentState) => ({
+      count: currentState.count + 1,
     }));
   };
 
   add100 = () => {
-    this.setState((state) => ({
-      count: state.count + 100,
+    this.setState((currentState) => ({
+      count: currentState.count + 100,
     }));
   };
 

2f0aadba21c4b410d865e370b0aa5ad227e46fa0
Vova-Stoliar <stoliarmoney@gmail.com>
2021-09-08 16:36:55 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,79 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  number: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state:State = {
+    number: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((currentState) => {
+      let { number } = currentState;
+
+      number += 1;
+
+      return {
+        number,
+      };
+    });
+  };
+
+  add100 = () => {
+    this.setState((currentState) => {
+      let { number } = currentState;
+
+      number += 100;
+
+      return {
+        number,
+      };
+    });
+  };
+
+  increase = () => {
+    this.setState((currentState) => {
+      let { number } = currentState;
+
+      number += number % 5 ? 1 : 100;
+
+      return {
+        number,
+      };
+    });
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          {`Count: ${this.state.number}`}
+        </h1>
+        <button
+          type="button"
+          className="button is-success is-focused"
+          onClick={() => this.addOne()}
+        >
+          add 1
+        </button>
+        <button
+          type="button"
+          className="button is-success is-focused"
+          onClick={() => this.add100()}
+        >
+          add 100
+        </button>
+        <button
+          type="button"
+          className="button is-success is-focused"
+          onClick={() => this.increase()}
+        >
+          increase
+        </button>
+      </>
     );
   }
 }

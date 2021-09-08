9a77d984c107c4d3d6205688db0dfd69d4a2fe26
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-09-08 09:34:08 +0300

    done task

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,85 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  initial: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    initial: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => ({
+      initial: prevState.initial + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({
+      initial: prevState.initial + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.initial % 5 === 0) {
+      this.add100();
+      this.addOne();
+    } else {
+      this.addOne();
+    }
+  };
+
+  reset = () => {
+    this.setState({ initial: 0 });
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div>
+        <div className="card">
+          <h1>
+            Count:
+            {' '}
+            {this.state.initial}
+          </h1>
+
+          <div>
+            <button
+              type="button"
+              onClick={this.addOne}
+              className="card-button"
+            >
+              Add one
+            </button>
+
+            <button
+              type="button"
+              onClick={this.add100}
+              className="card-button"
+            >
+              Add 100
+            </button>
+
+            <button
+              type="button"
+              onClick={this.increase}
+              className="card-button"
+            >
+              Add one + extra
+            </button>
+
+            <button
+              type="button"
+              onClick={this.reset}
+              className="card-button"
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }

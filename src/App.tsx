3afc0e6da66ed4190330e4e5560f462006922639
Dmitriy Pleshakov <dmitriypleshakov1@gmail.com>
2021-09-08 10:59:21 +0300

    React_Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Counter } from './components/Counter';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { count } = this.state;
+    const { addOne, add100, increase } = this;
+
     return (
-      <h1>Count: 0</h1>
+      <Counter
+        count={count}
+        addOne={addOne}
+        add100={add100}
+        increase={increase}
+      />
     );
   }
 }

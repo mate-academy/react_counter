114ea5553a0213655cc2bfeee80f7b979d1178d1
Kazkoviy <illyaboiko1807@gmail.com>
2021-09-09 01:41:32 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,78 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state: State = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => {
+      return {
+        counter: state.counter + 1,
+      };
+    });
+  };
+
+  add100 = () => {
+    this.setState((state) => {
+      return {
+        counter: state.counter + 100,
+      };
+    });
+  };
+
+  increase = () => {
+    this.setState((state) => {
+      const { counter } = state;
+
+      return {
+        counter: counter + (counter % 5 === 0 && counter !== 0 ? 101 : 1),
+      };
+    });
+  };
+
+  clear = () => {
+    this.setState({ counter: 0 });
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1>{`Count: ${this.state.counter}`}</h1>
+        <button
+          className="App__button"
+          type="button"
+          onClick={() => this.addOne()}
+        >
+          Add 1
+        </button>
+        <button
+          className="App__button"
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+        <button
+          className="App__button"
+          type="button"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+        <button
+          className="App__button"
+          type="button"
+          onClick={this.clear}
+        >
+          Clear
+        </button>
+      </div>
     );
   }
 }

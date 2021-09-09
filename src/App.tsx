a247dbb73bd74747338ce304c6bb5d5163c81706
Andriy Nikorych <andrei,nikorich@gmail.com>
2021-09-09 18:28:00 +0300

    create Counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,13 @@
 import React from 'react';
 import './App.scss';
+import { Counter } from './Components/Counter';
 
-type State = {
-  count: number;
+const App: React.FC = () => {
+  return (
+    <div className="counter">
+      <Counter />
+    </div>
+  );
 };
-
-class App extends React.Component<{}, State> {
-  state = {
-    count: 0,
-  };
-
-  addOne = () => {
-    this.setState((state) => ({
-      count: state.count + 1,
-    }));
-  };
-
-  add100 = () => {
-    this.setState((state) => ({
-      count: state.count + 100,
-    }));
-  };
-
-  increase = () => {
-    this.addOne();
-
-    if (this.state.count % 5 === 0) {
-      this.add100();
-    }
-  };
-
-  render() {
-    const { count } = this.state;
-    const message = `Count: ${count}`;
-
-    return (
-      <div className="counter">
-        <h1>{message}</h1>
-        <div className="container">
-          <button type="button" onClick={this.addOne}>
-            Add one
-          </button>
-
-          <button type="button" onClick={this.add100}>
-            Аdd 100
-          </button>
-
-          <button type="button" onClick={this.increase}>
-            Increase
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
 
 export default App;

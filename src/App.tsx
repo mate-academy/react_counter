7cf2f2615e0fe4056e641213293ee624f8c6f815
Ihor Shapoval <123sgor.shapoval@gmail.com>
2021-09-09 13:46:15 +0300

    Little Fix

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,21 @@ export class App extends React.Component<{}, State> {
   };
 
   addOne = () => {
-    this.setState((state) => ({
-      count: state.count + 1,
+    this.setState((currentState) => ({
+      count: currentState.count + 1,
     }));
   };
 
   add100 = () => {
-    this.setState((state) => ({
-      count: state.count + 100,
+    this.setState((currentState) => ({
+      count: currentState.count + 100,
     }));
   };
 
   increase = () => {
     const { count } = this.state;
 
-    if (count % 5 === 0 && count > 0) {
+    if (count % 5 === 0) {
       this.add100();
     }
 

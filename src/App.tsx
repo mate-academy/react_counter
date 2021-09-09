37bd945a28ff637f50e6abb38e0caf1a02fbe249
Roman Yacenko <ryacenko3@gmail.com>
2021-09-09 10:23:40 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,61 @@
-import React from 'react';
+import * as React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  sum: number,
+  counter: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    sum: 0,
+    counter: 5,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      sum: state.sum + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({
+      sum: state.sum + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState((state) => {
+      if (state.counter !== 1) {
+        return {
+          sum: state.sum + 1,
+          counter: state.counter - 1,
+        };
+      }
+
+      return {
+        sum: state.sum + 5,
+        counter: state.counter + 5,
+      };
+    });
+  };
 
   render() {
+    const { sum } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="main">
+        <h1>
+          Count:
+          {sum}
+        </h1>
+        <div className="container">
+          <button type="button" className="btn btn-success" onClick={this.addOne}>+1</button>
+          <button type="button" className="btn btn-success" onClick={this.add100}>+100</button>
+          <button type="button" className="btn btn-success" onClick={this.increase}>increase</button>
+        </div>
+      </div>
+
     );
   }
 }

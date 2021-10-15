74734cdd1ab97fcf4a5f260baf4546d1959c8565
unknown <shulzhenkovlad01@gmail.com>
2021-10-15 13:53:37 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,69 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.counter % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>{`Count: ${this.state.counter}`}</h1>
+        <div className="buttons">
+          <button
+            className="buttons__button"
+            type="button"
+            onClick={() => {
+              this.addOne();
+            }}
+          >
+            Add 1
+          </button>
+          <button
+            className="buttons__button"
+            type="button"
+            onClick={() => {
+              this.add100();
+            }}
+          >
+            Add 100
+          </button>
+          <button
+            className="buttons__button"
+            type="button"
+            onClick={() => {
+              this.increase();
+            }}
+          >
+            Smart add
+          </button>
+        </div>
+      </>
     );
   }
 }

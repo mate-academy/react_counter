a5d9152849a2a82b542b831d29b71d677e4b3bcf
Ruslan-Mykhalchak <ruslan1993x@gmail.com>
2021-10-15 17:29:50 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,55 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState((prevState) => ({
+      count: prevState.count % 5 === 0 ? (prevState.count + 100) : (prevState.count + 1),
+    }));
+  };
+
+  reset = () => {
+    this.setState((prevState) => ({
+      count: prevState.count * 0,
+    }));
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="container">
+        <h1 className="title">
+          Count $
+          {count}
+        </h1>
+        <div className="buttons">
+          <button className="button" type="button" onClick={this.addOne}>+1</button>
+          <button className="button" type="button" onClick={this.add100}>+100</button>
+          <button className="button" type="button" onClick={this.increase}>Increase</button>
+          <button className="button" type="button" onClick={this.reset}>Reset</button>
+        </div>
+      </div>
     );
   }
 }

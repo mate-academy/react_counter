861ac136e766a8af71cfb71d0b6dd59f13100b42
Maksym Vikolash <vikolashmaxim@gmail.com>
2021-10-18 12:04:07 +0300

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.scss';
 
 type State = {
   count: number;
-}
+};
 
 class Counter extends Component<{}, State> {
   state: State = {
     count: 0,
-  }
+  };
+
   addOne = () => {
     this.setState((state) => ({
       count: state.count + 1,
@@ -22,33 +23,39 @@ class Counter extends Component<{}, State> {
   };
 
   increase = () => {
-    this.setState((state) => ({
-      count: state.count + (
-        (state.count % 5 === 0) ? 101 : 1
-      ),
-    }));
+    this.setState((state) => {
+      if (state.count % 5 === 0) {
+        this.add100();
+      }
+
+      this.addOne();
+    });
   };
 
   render() {
     const { count } = this.state;
+
     return (
       <div className="App">
-        <h1>Count: {count}</h1>
+        <h1>
+          Count:
+          {count}
+        </h1>
 
         <button
-          type='button'
+          type="button"
           onClick={this.addOne}
         >
           This button adds 1!
         </button>
         <button
-          type='button'
+          type="button"
           onClick={this.add100}
         >
           This button adds 100!
         </button>
         <button
-          type='button'
+          type="button"
           onClick={this.increase}
         >
           This button DONT just add 1!

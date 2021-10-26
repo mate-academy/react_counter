b3ddf259ab2da4c56ef260d592392724e6851ee2
Anton Forzun <anton_forzun@ukr.net>
2021-10-26 14:17:43 +0300

    react-counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,74 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+interface State {
+  num: number,
+}
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    num: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      num: state.num + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({
+      num: state.num + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.num % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
+
+  reset = () => {
+    this.setState({
+      num: 0,
+    });
+  };
 
   render() {
+    const { num } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1>
+          {`Count: ${num}`}
+        </h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          addOne
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          add100
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          increase
+        </button>
+        <button
+          type="button"
+          onClick={this.reset}
+        >
+          RESET
+        </button>
+      </div>
     );
   }
 }

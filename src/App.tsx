4afcdaec4e6b249bddef8cf6e83a5887c9501344
Olga Bordiug <obordiug@gmail.com>
2021-11-09 13:12:02 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import './App.scss';
+import { Counter } from './components/Counter';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
-  addOne = () => {};
-
-  add100 = () => {};
-
-  increase = () => {};
-
   render() {
     return (
-      <h1>Count: 0</h1>
+      <Counter />
     );
   }
 }

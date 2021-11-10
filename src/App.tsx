6149de70a894937c5ccf75d1bfd038130dc3d7f3
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-10 17:20:15 +0300

    Task was solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,68 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type Props = {};
+interface State {
+  counter: number;
+}
 
-  add100 = () => {};
+class App extends React.Component<Props, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState(prevState => (
+      { counter: prevState.counter + 1 }
+    ));
+  };
+
+  add100 = () => {
+    this.setState(prevState => (
+      { counter: prevState.counter + 100 }
+    ));
+  };
+
+  increase = () => {
+    const { counter } = this.state;
+
+    if (counter && counter % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="counter">
+        <h1 className="counter__title">{`Count: ${this.state.counter}`}</h1>
+        <div className="counter__form">
+          <button
+            type="button"
+            className="counter__button"
+            onClick={this.addOne}
+          >
+            Add 1
+          </button>
+
+          <button
+            type="button"
+            className="counter__button"
+            onClick={this.add100}
+          >
+            Add 100
+          </button>
+
+          <button
+            type="button"
+            className="counter__button"
+            onClick={this.increase}
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

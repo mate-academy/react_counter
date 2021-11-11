e3413228648a3677b3c694ee2820c0809a48ce1a
Misha Rosa <misharosa.lviv@gmail.com>
2021-11-11 13:03:31 +0200

    Adder was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+interface State {
+  count: number;
+}
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state:State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    let { count } = this.state;
+
+    this.setState({ count: count += 1 });
+  };
+
+  add100 = () => {
+    let { count } = this.state;
+
+    this.setState({ count: count += 100 });
+  };
+
+  increase = () => {
+    let { count } = this.state;
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    (count % 5 === 0 && count !== 0)
+      ? this.setState({ count: count += 101 })
+      : this.setState({ count: count += 1 });
+  };
+
+  cleaner = () => {
+    let { count } = this.state;
+
+    this.setState({ count: count *= 0 });
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {count}
+        </h1>
+        <button type="button" onClick={this.addOne}>
+          add 1
+        </button>
+
+        <button type="button" onClick={this.add100}>
+          add 100
+        </button>
+
+        <button type="button" onClick={this.increase}>
+          add 1 or 100
+        </button>
+
+        <button type="button" onClick={this.cleaner}>
+          Clean
+        </button>
+      </>
     );
   }
 }

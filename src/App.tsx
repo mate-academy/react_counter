67b4ccc1c8e5c171ed4da85c718f325fecf9ebda
Ivarovskiy <iva7art@gmail.com>
2021-11-15 16:36:49 +0400

    fixes solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,10 @@
-import React from 'react';
 import './App.scss';
+import { Counter } from './components/Counter';
 
-type Props = {
-
+const App = () => {
+  return (
+    <Counter />
+  );
 };
-
-interface State {
-  count: number;
-}
-
-class App extends React.Component<Props, State> {
-  state: State = {
-    count: 0,
-  };
-
-  addOne = () => {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + 1,
-      };
-    });
-  };
-
-  add100 = () => {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + 100,
-      };
-    });
-  };
-
-  increase = () => {
-    const { count } = this.state;
-
-    this.addOne();
-    if (count % 5 === 0 && count !== 0) {
-      this.add100();
-    }
-  };
-
-  render() {
-    const { count } = this.state;
-
-    return (
-      <>
-        <h1>
-          Count:
-          {count}
-        </h1>
-        <button type="button" onClick={this.addOne}>
-          Add 1
-        </button>
-        <button type="button" onClick={this.add100}>
-          Add 100
-        </button>
-        <button type="button" onClick={this.increase}>
-          Increase
-        </button>
-      </>
-    );
-  }
-}
 
 export default App;

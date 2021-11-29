0672891b4c5b90d185e8deed1e5d856c8a4804d7
Taras Pyda <tapac1991@icloud.com>
2021-11-29 17:32:18 +0200

    Fixes after code review

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 type State = {
   count: number,
-  increaseClicksCount: number,
 };
 
 export class Counter extends React.Component<{}, State> {
   state = {
     count: 0,
-    increaseClicksCount: 0,
   };
 
   addOne = () => {
@@ -24,23 +22,18 @@ export class Counter extends React.Component<{}, State> {
   };
 
   increase = () => {
-    this.addOne();
-    this.setState((state) => {
-      return ({ increaseClicksCount: state.increaseClicksCount + 1 });
-    });
+    const { count } = this.state;
 
-    if (this.state.increaseClicksCount === 5) {
+    this.addOne();
+    if (count % 5 === 0 && count !== 0) {
       this.add100();
-      this.setState({ increaseClicksCount: 1 });
     }
   };
 
   render() {
-    const { count } = this.state;
-
     return (
       <>
-        <h1>{`Count: ${count}`}</h1>
+        <h1>{`Count: ${this.state.count}`}</h1>
         <button type="button" onClick={this.addOne}>+ 1</button>
         <button type="button" onClick={this.add100}>+ 100</button>
         <button type="button" onClick={this.increase}>Increase</button>

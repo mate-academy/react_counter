21d001e775bdf45018a495a58e9aee7625fa4db6
Dariia Romanova <dariia.romanova@gmail.com>
2021-12-10 16:36:27 +0200

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,8 @@ import React from 'react';
 import './App.scss';
 import { Counter } from './components/Counter';
 
-type State = {
-  currentCount: number,
-};
-
-class App extends React.Component<{}, State> {
-  addOne = () => {};
-
-  add100 = () => {};
-
-  increase = () => {};
-
-  render() {
-    return (
-      <Counter />
-    );
-  }
-}
+const App: React.FC = () => (
+  <Counter />
+);
 
 export default App;

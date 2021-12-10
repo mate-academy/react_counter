b8046b9d3179c8921db4d17181a3d7f438ef55f6
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-12-10 16:32:53 +0200

    component was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,13 @@
 import React from 'react';
 import './App.scss';
+import { Counter } from './components/Counter';
 
-type State = {
-  counter: number,
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 };
-
-class App extends React.Component<{}, State> {
-  state = {
-    counter: 0,
-  };
-
-  addOne = () => {
-    this.setState((prevState) => {
-      return { counter: prevState.counter + 1 };
-    });
-  };
-
-  add100 = () => {
-    this.setState((prevState) => {
-      return { counter: prevState.counter + 100 };
-    });
-  };
-
-  increase = () => {
-    this.setState((prevState) => {
-      if (prevState.counter % 5 === 0) {
-        return (this.add100(), this.addOne());
-      }
-
-      return this.addOne();
-    });
-  };
-
-  render() {
-    const { counter } = this.state;
-
-    return (
-      <div className="countSection">
-        <h1 className="countSection__title">
-          Add more years to your life!
-        </h1>
-        <h1 className="countSection__count">
-          Count:
-          {counter}
-        </h1>
-        <div className="countSection__buttons">
-          <button type="button" className="countSection__button1" onClick={this.addOne}>
-            Add One
-          </button>
-          <button type="button" className="countSection__button2" onClick={this.add100}>
-            Add Hundred
-          </button>
-          <button type="button" className="countSection__button3" onClick={this.increase}>
-            Add More
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
 
 export default App;

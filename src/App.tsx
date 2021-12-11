4239f64a599ca17426aad304f12f97a448d174fd
Oleksiy Pochkayev <oleksiypochkayev@gmail.com>
2021-12-12 00:31:21 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,151 @@
 import React from 'react';
 import './App.scss';
+import './calculator.scss';
 
 type State = {
   count: number;
+  num: string;
+  calculateSign: string;
+  calcNums: [] | string[] | (number | string)[]
 };
 
 class App extends React.Component<{}, State> {
   state = {
     count: 0,
+    num: '',
+    calculateSign: '',
+    calcNums: [],
+  };
+
+  equal = () => {
+    this.setState((prevState) => {
+      const result:(number | string)[] = [...prevState.calcNums, Number(prevState.num)];
+
+      for (let i = 0; i < result.length; i += 1) {
+        if (result[i] === '*') {
+          const numb = (result[i - 1] as number) * (result[i + 1] as number);
+
+          result.splice(i - 1, 3, numb);
+          i = 0;
+        } else if (result[i] === '/') {
+          const numb = (result[i - 1] as number) / (result[i + 1] as number);
+
+          result.splice(i - 1, 3, numb);
+          i = 0;
+        }
+      }
+
+      for (let i = 0; i < result.length; i += 1) {
+        if (result[i] === '+') {
+          const numb = (result[i - 1] as number) + (result[i + 1] as number);
+
+          result.splice(i - 1, 3, numb);
+          i = 0;
+        } else if (result[i] === '-') {
+          const numb = (result[i - 1] as number) - (result[i + 1] as number);
+
+          result.splice(i - 1, 3, numb);
+          i = 0;
+        }
+      }
+
+      return {
+        num: String(result[0]),
+        calcNums: [],
+      };
+    });
+  };
+
+  clear = () => {
+    this.setState({ calcNums: [], num: '' });
+  };
+
+  addition = () => {
+    this.setState((prev) => ({
+      calcNums: [...prev.calcNums, Number(prev.num), '+'],
+      num: '',
+    }));
+  };
+
+  subtraction = () => {
+    this.setState((prev) => ({
+      calcNums: [...prev.calcNums, Number(prev.num), '-'],
+      num: '',
+    }));
+  };
+
+  multiplication = () => {
+    this.setState((prev) => ({
+      calcNums: [...prev.calcNums, Number(prev.num), '*'],
+      num: '',
+    }));
+  };
+
+  division = () => {
+    this.setState((prev) => ({
+      calcNums: [...prev.calcNums, Number(prev.num), '/'],
+      num: '',
+    }));
+  };
+
+  zero = () => {
+    this.setState((prev) => ({
+      num: prev.num + 0,
+    }));
+  };
+
+  one = () => {
+    this.setState((prev) => ({
+      num: prev.num + 1,
+    }));
+  };
+
+  two = () => {
+    this.setState((prev) => ({
+      num: prev.num + 2,
+    }));
+  };
+
+  three = () => {
+    this.setState((prev) => ({
+      num: prev.num + 3,
+    }));
+  };
+
+  four = () => {
+    this.setState((prev) => ({
+      num: prev.num + 4,
+    }));
+  };
+
+  five = () => {
+    this.setState((prev) => ({
+      num: prev.num + 5,
+    }));
+  };
+
+  six = () => {
+    this.setState((prev) => ({
+      num: prev.num + 6,
+    }));
+  };
+
+  seven = () => {
+    this.setState((prev) => ({
+      num: prev.num + 7,
+    }));
+  };
+
+  eight = () => {
+    this.setState((prev) => ({
+      num: prev.num + 8,
+    }));
+  };
+
+  nine = () => {
+    this.setState((prev) => ({
+      num: prev.num + 9,
+    }));
   };
 
   addOne = () => {
@@ -43,66 +181,250 @@ class App extends React.Component<{}, State> {
   };
 
   render() {
-    const { count } = this.state;
+    const {
+      count, num, calculateSign, calcNums,
+    } = this.state;
+    const isFirstEmpty = num.length !== 0;
+    const isNumEmpty = num.length === 0;
 
     return (
       <div className="app">
-        <h1 className="app__numbers">
-          Count:
-          {' '}
-          { count }
-        </h1>
+        <div className="calculator">
+          <div className="calculator__display">
+            <div className="calculator__numbers">
+              {calcNums.map((item) => (
+                `${item} `
+              ))}
+              {num}
+            </div>
+          </div>
 
-        <div className="app__buttons">
-          <button
-            className="app__button"
-            type="button"
-            onClick={() => {
-              this.addOne();
-            }}
-          >
-            +1
-          </button>
+          <div className="calculator__buttons">
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.seven();
+              }}
+            >
+              7
+            </button>
 
-          <button
-            className="app__button"
-            type="button"
-            onClick={() => {
-              this.add100();
-            }}
-          >
-            +100
-          </button>
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.eight();
+              }}
+            >
+              8
+            </button>
 
-          <button
-            className="app__button"
-            type="button"
-            onClick={() => {
-              this.increase();
-            }}
-          >
-            Increase
-          </button>
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.nine();
+              }}
+            >
+              9
+            </button>
 
-          <button
-            className="app__button"
-            type="button"
-            onClick={() => {
-              this.random();
-            }}
-          >
-            Random
-          </button>
+            <button
+              className="app__button"
+              type="button"
+              disabled={!isFirstEmpty}
+              onClick={() => {
+                this.division();
+              }}
+            >
+              ÷
+            </button>
 
-          <button
-            className="app__button"
-            type="button"
-            onClick={() => {
-              this.reset();
-            }}
-          >
-            Reset
-          </button>
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.four();
+              }}
+            >
+              4
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.five();
+              }}
+            >
+              5
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.six();
+              }}
+            >
+              6
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              disabled={!isFirstEmpty}
+              onClick={() => {
+                this.multiplication();
+              }}
+            >
+              X
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.one();
+              }}
+            >
+              1
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.two();
+              }}
+            >
+              2
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.three();
+              }}
+            >
+              3
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              disabled={!isFirstEmpty}
+              onClick={() => {
+                this.subtraction();
+              }}
+            >
+              -
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              disabled={isNumEmpty}
+              onClick={() => {
+                this.zero();
+              }}
+            >
+              0
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.clear();
+              }}
+            >
+              C
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.equal();
+              }}
+            >
+              =
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              disabled={!isFirstEmpty}
+              onClick={() => {
+                this.addition();
+              }}
+            >
+              +
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <h1 className="app__numbers">
+            Count:
+            {' '}
+            { count }
+          </h1>
+
+          <div className="app__buttons">
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.addOne();
+              }}
+            >
+              +1
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.add100();
+              }}
+            >
+              +100
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.increase();
+              }}
+            >
+              Increase
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.random();
+              }}
+            >
+              Random
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={() => {
+                this.reset();
+              }}
+            >
+              Reset
+            </button>
+          </div>
         </div>
       </div>
     );

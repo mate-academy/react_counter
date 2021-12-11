7a719669d4f858955589d706bd4eb3188ddcd8cb
Oleksiy Pochkayev <oleksiypochkayev@gmail.com>
2021-12-11 18:50:29 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,110 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prev) => ({
+      count: prev.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prev) => ({
+      count: prev.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0 && this.state.count > 0) {
+      this.add100();
+    }
+  };
+
+  reset = () => {
+    this.setState({ count: 0 });
+  };
+
+  random = () => {
+    const random = Math.round(Math.random() * 100);
+
+    this.setState((prev) => ({
+      count: prev.count + random,
+    }));
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="app">
+        <h1 className="app__numbers">
+          Count:
+          {' '}
+          { count }
+        </h1>
+
+        <div className="app__buttons">
+          <button
+            className="app__button"
+            type="button"
+            onClick={() => {
+              this.addOne();
+            }}
+          >
+            +1
+          </button>
+
+          <button
+            className="app__button"
+            type="button"
+            onClick={() => {
+              this.add100();
+            }}
+          >
+            +100
+          </button>
+
+          <button
+            className="app__button"
+            type="button"
+            onClick={() => {
+              this.increase();
+            }}
+          >
+            Increase
+          </button>
+
+          <button
+            className="app__button"
+            type="button"
+            onClick={() => {
+              this.random();
+            }}
+          >
+            Random
+          </button>
+
+          <button
+            className="app__button"
+            type="button"
+            onClick={() => {
+              this.reset();
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     );
   }
 }

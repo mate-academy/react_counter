415e91b3fa96b13615185c412aa89a5a7ea60f3b
Artem Skachkov <erocezz@gmail.com>
2021-12-13 00:28:00 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,73 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class Counter extends Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState((state) => {
+      if (state.count % 5 === 0) {
+        this.add100();
+      }
+
+      this.addOne();
+    });
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1
+          className="App__title"
+        >
+          Count:
+          {count}
+        </h1>
+
+        <button
+          type="button"
+          onClick={this.addOne}
+          className="App__button"
+        >
+          Click me for add 1
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+          className="App__button"
+        >
+          Click me for add 100
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+          className="App__button"
+        >
+          Click me five times to see something special
+        </button>
+      </div>
     );
   }
 }
 
-export default App;
+export default Counter;

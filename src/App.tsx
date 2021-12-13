24fd0fb113f7ddfbebd52d7d3b2c8fbf2fe33628
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-12-13 17:14:54 +0200

    without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,66 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
+
+  addOne = () => {
+    this.setState(state => ({
+      counter: state.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({
+      counter: state.counter + 100,
+    }));
+  };
 
   increase = () => {};
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1>
+          Count:
+          {this.state.counter}
+        </h1>
+        <button
+          type="button"
+          onClick={() => {
+            this.add100();
+          }}
+        >
+          Add 100
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            this.addOne();
+          }}
+        >
+          Add One
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            this.addOne();
+            if (this.state.counter % 5 === 0) {
+              this.add100();
+            }
+          }}
+        >
+          Special Counter
+        </button>
+      </div>
     );
   }
 }

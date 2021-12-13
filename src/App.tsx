53137089023c7f90e3407b6b90865b7b65dc1a71
nadiaH <nadiahrb79@gmailcom>
2021-12-13 16:06:44 +0200

    solution without style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,63 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState(state => {
+      if (state.count % 5 === 0) {
+        return this.add100();
+      }
+
+      return this.addOne();
+    });
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1 className="App__title">{`Count: ${this.state.count}`}</h1>
+        <button
+          type="button"
+          className="App__btn"
+          onClick={() => this.addOne()}
+        >
+          Add One
+        </button>
+        <button
+          type="button"
+          className="App__btn"
+          onClick={() => this.add100()}
+        >
+          Add 100
+        </button>
+        <button
+          type="button"
+          className="App__btn"
+          onClick={() => this.increase()}
+        >
+          Increase
+        </button>
+      </div>
     );
   }
 }

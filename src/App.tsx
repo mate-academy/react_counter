db037bc78f0acf885b226665d3d21542609ac654
workacccom <workacccom@gmail.com>
2021-12-13 13:04:21 +0200

    First solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
 import React from 'react';
 import './App.scss';
+import { State } from './State';
 
 class App extends React.Component {
-  addOne = () => {};
+  state = {
+    sum: 0,
+  };
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState((state: State) => ({
+      sum: state.sum + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState((state: State) => ({
+      sum: state.sum + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.sum % 5 === 0) {
+      this.addOne();
+      this.add100();
+    } else {
+      this.addOne();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1 className="App__title">
+          {`Count: ${this.state.sum}`}
+        </h1>
+
+        <div className="App__buttons">
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.addOne}
+          >
+            +1
+          </button>
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.add100}
+          >
+            +100
+          </button>
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.increase}
+          >
+            increase
+          </button>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/State.tsx
@@ -0,0 +1,3 @@
+export interface State {
+  sum: number;
+}

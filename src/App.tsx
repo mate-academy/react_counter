826e0b0699cea2ec7c2d74fad75c20a4413dc073
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-12-13 12:19:14 +0200

    implemented task

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1 className="App__title">{`Count ${this.state.count}`}</h1>
+        <div className="buttons">
+          <button type="button" className="buttons__item" onClick={() => this.addOne()}>Add 1</button>
+          <button type="button" className="buttons__item" onClick={() => this.add100()}>Add 100</button>
+          <button type="button" className="buttons__item" onClick={() => this.increase()}>Increase</button>
+        </div>
+      </div>
     );
   }
 }

e96a012be6cacb0b553867c38675b2db31ed9a69
Anton Kulchytskyi <antek1809@gmail.com>
2021-12-29 12:36:16 +0100

    react counter solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,69 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  num: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    num: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      num: state.num + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      num: state.num + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+    if (this.state.num % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { num } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1 className="title__count">
+          Count:
+          {' '}
+          {num}
+        </h1>
+
+        <button
+          type="button"
+          className="button__count"
+          onClick={this.addOne}
+        >
+          Add one!
+        </button>
+
+        <button
+          type="button"
+          className="button__count"
+          onClick={this.add100}
+        >
+          Add one hundred!
+        </button>
+
+        <button
+          type="button"
+          className="button__count"
+          onClick={this.increase}
+        >
+          Add increase!
+        </button>
+      </>
     );
   }
 }

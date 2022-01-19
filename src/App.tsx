4d9a1874d0731117a622c61f74637c23b9f2eecc
Nikolas <tes.kolia@gmail.com>
2022-01-19 23:20:09 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+export default class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState((state) => ({ count: state.count + 1 }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState((state) => ({ count: state.count + 100 }));
+  };
+
+  increase = () => {
+    this.setState((state) => {
+      if (state.count % 5 === 0) {
+        this.add100();
+      }
+
+      return { count: state.count + 1 };
+    });
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>{`Count: ${this.state.count}`}</h1>
+        <button type="button" onClick={this.addOne}>Add onde</button>
+        <button type="button" onClick={this.add100}>Add 100</button>
+        <button type="button" onClick={this.increase}>Increase</button>
+      </>
     );
   }
 }
-
-export default App;

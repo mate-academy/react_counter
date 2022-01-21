7384e0807a77c33fd314f754382834463e9f0a08
Yulia Koshkina <julyasav4uk@gmail.com>
2022-01-21 18:43:23 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <div className="App__counter counter">
+          <div className="ui black segment">
+            <h1 className="header">{this.state.count}</h1>
+            <div className="ui buttons">
+              <button
+                type="button"
+                className="ui button"
+                onClick={this.addOne}
+              >
+                AddOne
+              </button>
+              <button
+                type="button"
+                className="ui button"
+                onClick={this.add100}
+              >
+                Add100
+              </button>
+              <button
+                type="button"
+                className="ui button"
+                onClick={this.increase}
+              >
+                Increase
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }

943e342f0baa1c96f0ff1dcd05ad358632e84372
Andrii Koptiev <koptev.andrey87@gmail.com>
2022-01-21 11:12:54 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,68 @@
 import React from 'react';
+import { Button } from '@mui/material';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state: State = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.counter % 5 === 0 || this.state.counter === 0) {
+      this.addOne();
+      this.add100();
+    } else {
+      this.addOne();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <div className="counter App__counter">
+          <h1 className="counter__number">{this.state.counter}</h1>
+          <div className="counter__button-container">
+            <Button
+              className="button"
+              variant="contained"
+              onClick={this.addOne}
+            >
+              add1
+            </Button>
+            <Button
+              className="button"
+              variant="contained"
+              onClick={this.add100}
+            >
+              add100
+            </Button>
+            <Button
+              className="button"
+              variant="contained"
+              onClick={this.increase}
+            >
+              increase
+            </Button>
+
+          </div>
+        </div>
+      </div>
     );
   }
 }

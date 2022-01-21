eadfc9dc4f79172a47ba50ec8872323318eb2e38
Marina Tilniak <marinatilnak@gmail.com>
2022-01-22 00:54:37 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
 import React from 'react';
+import 'bulma/css/bulma.min.css';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1 className="title">{`Count: ${count}`}</h1>
+
+        <button
+          type="button"
+          onClick={this.addOne}
+          className="button m-2"
+        >
+          Add One
+        </button>
+
+        <button
+          type="button"
+          onClick={this.add100}
+          className="button m-2"
+        >
+          Add 100
+        </button>
+
+        <button
+          type="button"
+          onClick={this.increase}
+          className="button m-2"
+        >
+          Increase
+        </button>
+      </>
     );
   }
 }

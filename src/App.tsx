71d8c0ceee3fc0d1d228332ddbc0032d11e626a8
Mikedacheka <mikedacheka@gmail.com>
2022-01-23 01:10:45 +0200

    sm msg

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  number: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state: State = {
+    number: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState(prevState => ({
+      number: prevState.number + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(prevState => ({
+      number: prevState.number + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.number % 5 === 0) {
+      this.add100();
+      this.addOne();
+    } else {
+      this.addOne();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {' '}
+          {this.state.number}
+        </h1>
+        <button type="button" onClick={this.addOne}>
+          addOne
+        </button>
+        <button type="button" onClick={this.add100}>
+          add100
+        </button>
+        <button type="button" onClick={this.increase}>
+          increase
+        </button>
+      </>
     );
   }
 }

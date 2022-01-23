5b97c3c95329b9a754f6d2a952e91b87af717cc1
Yevhenii Korzun <marty.music.official@gmail.com>
2022-01-23 22:55:48 +0200

    beta: logic implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+export class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  };
+
+  add100 = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 100,
+      };
+    });
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>{`Count: ${this.state.count}`}</h1>
+        <button type="button" onClick={this.addOne}>AddOne</button>
+        <button type="button" onClick={this.add100}>Add100</button>
+        <button type="button" onClick={this.increase}>Increase</button>
+      </>
     );
   }
 }
-
-export default App;

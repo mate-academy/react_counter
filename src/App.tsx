5a143abd52cb2370e8ce23e5f871ce20ae141718
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2022-01-23 19:04:15 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,87 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.setState(() => {
+        return this.add100();
+      });
+    }
+
+    this.setState(() => {
+      return this.addOne();
+    });
+  };
+
+  resetCounter = () => {
+    this.setState((state) => ({
+      count: state.count * 0,
+    }));
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {' '}
+          {this.state.count}
+        </h1>
+        <button
+          className="button"
+          type="button"
+          onClick={this.addOne}
+        >
+          +1
+        </button>
+        {' '}
+        <button
+          className="button"
+          type="button"
+          onClick={this.add100}
+        >
+          +100
+        </button>
+        {' '}
+        <button
+          className="button"
+          type="button"
+          onClick={this.increase}
+        >
+          Strange button
+        </button>
+        {' '}
+        <button
+          className="button button--resetButton"
+          type="button"
+          onClick={() => {
+            this.resetCounter();
+          }}
+        >
+          Reset counter
+        </button>
+      </>
     );
   }
 }

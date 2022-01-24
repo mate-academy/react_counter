4c38405426089932d9f7158b83b104cdfe43eb52
Nikita Polishchuk <theetempleovpsychicyouth@gmail.com>
2022-01-24 17:04:45 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type Props = {};
 
-  add100 = () => {};
+type State = {
+  displayedNumber: number;
+};
 
-  increase = () => {};
+class App extends React.Component<Props, State> {
+  state: State = {
+    displayedNumber: 0,
+  };
+
+  addOne = () => {
+    this.setState((state) => ({
+      displayedNumber: state.displayedNumber + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      displayedNumber: state.displayedNumber + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.displayedNumber % 5 === 0
+      && this.state.displayedNumber !== 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="app">
+        <h1 className="title">{`Count: ${this.state.displayedNumber}`}</h1>
+
+        <button
+          type="button"
+          className="button is-info"
+          onClick={this.addOne}
+        >
+          Add one
+        </button>
+
+        <button
+          type="button"
+          className="button is-success"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+
+        <button
+          type="button"
+          className="button is-warning"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+      </div>
     );
   }
 }

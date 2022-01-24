7c392c4efc0d6c6de3c2a4941361fa49a8350cf4
Taras Zherebetskiy <tarasj76@gmail.com>
2022-01-24 12:03:45 +0200

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,75 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  };
+
+  add100 = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 100,
+      };
+    });
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1
+          className="title"
+        >
+          Count:
+          {` ${count}`}
+        </h1>
+
+        <br />
+
+        <button
+          type="button"
+          className="button"
+          onClick={this.addOne}
+        >
+          addOne
+        </button>
+        <button
+          type="button"
+          className="button"
+          onClick={this.add100}
+        >
+          add100
+        </button>
+        <button
+          type="button"
+          className="button"
+          onClick={this.increase}
+        >
+          addOne
+        </button>
+      </>
     );
   }
 }

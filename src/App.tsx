cb8db485999e4da61d7d9625f77fc407513c2f7b
Vova Alputov <alputov1998@gmail.com>
2022-01-25 18:50:57 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  };
+
+  add100 = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 100,
+      };
+    });
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div>
+        <h1>
+          Count:
+          {' '}
+          {count}
+        </h1>
+        <button type="submit" onClick={this.addOne}>1</button>
+        <button type="submit" onClick={this.add100}>100</button>
+        <button type="submit" onClick={this.increase}>increase</button>
+      </div>
     );
   }
 }

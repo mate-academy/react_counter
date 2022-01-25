8d4557d3d5f749c848b34ae1f3c63b0708ff6eac
levix2l <alexleviz13@gmail.com>
2022-01-25 15:36:10 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import React from 'react';
 import 'bulma/css/bulma.min.css';
 
 import { Counter } from './components/Counter';
 
-class App extends React.Component {
-  state = {};
-
-  render() {
-    return (
-      <div className="box">
-        <Counter />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="box">
+    <Counter />
+  </div>
+);
 
 export default App;

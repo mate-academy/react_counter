0b11e3177455bff132fca358d5b28a5aca42fb8d
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-02-14 23:46:54 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,74 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+interface Counter {
+  counter: number,
+}
 
-  add100 = () => {};
+class App extends React.Component<{}, Counter> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prev) => {
+      const result = prev.counter + 1;
+
+      return ({ counter: result });
+    });
+  };
+
+  add100 = () => {
+    this.setState((prev) => {
+      const result = prev.counter + 100;
+
+      return ({ counter: result });
+    });
+  };
+
+  increase = () => {
+    this.setState(({ counter }) => {
+      const adder = counter % 5 === 0 ? 100 : 1;
+      const result = counter + adder;
+
+      return ({ counter: result });
+    });
+  };
 
   render() {
+    const btnClass = 'waves-effect waves-purple btn-large';
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>{`Count: ${this.state.counter}`}</h1>
+        <button
+          className={btnClass}
+          type="button"
+          onClick={() => {
+            this.addOne();
+          }}
+        >
+          add One
+        </button>
+        <button
+          className={btnClass}
+          type="button"
+          onClick={() => {
+            this.add100();
+          }}
+        >
+          add 100
+        </button>
+        <button
+          className={btnClass}
+          type="button"
+          onClick={() => {
+            this.increase();
+          }}
+        >
+          increase c:
+        </button>
+      </>
     );
   }
 }

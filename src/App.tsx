4731ab64550ed23901287daa9f03a77f470f4718
Juliya <jursylian@gmail.com>
2022-02-17 23:48:31 +0200

    first solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.scss';
 
+import { Counter } from './components/Counter/Counter';
+
+// eslint-disable-next-line
 class App extends React.Component {
-  addOne = () => {};
-
-  add100 = () => {};
-
-  increase = () => {};
-
   render() {
     return (
-      <h1>Count: 0</h1>
+      <Counter />
     );
   }
 }

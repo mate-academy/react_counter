2fc4361403f84d39608b28be551a8632740b3941
Kozhukhovskyi Roman <4kroman@gmail.com>
2022-02-17 10:17:24 +0200

    soltion

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = { count: number };
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state: State) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state: State) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    if (count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {' '}
+          { count }
+        </h1>
+        <button
+          className="products__btn"
+          type="button"
+          onClick={this.addOne}
+        >
+          Add 1
+        </button>
+        <button
+          className="products__btn"
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+        <button
+          className="products__btn"
+          type="button"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+      </>
     );
   }
 }

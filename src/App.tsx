bb84fc91efbfd9c04f88820bff4c47e630587991
maxim2310 <m23a09x96@gmail.com>
2022-02-17 22:54:38 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+    this.setState((state) => {
+      if (state.count % 5 === 0) {
+        this.add100();
+      }
+    });
+  };
 
   render() {
+    const { count } = this.state;
+    const btnClass = 'waves-effect waves-purple btn-large';
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>{`Count: ${count}`}</h1>
+        <div>
+          <button
+            type="button"
+            onClick={this.addOne}
+            className={btnClass}
+          >
+            addOne
+          </button>
+          <button
+            type="button"
+            onClick={this.add100}
+            className={btnClass}
+          >
+            add 100
+          </button>
+          <button
+            type="button"
+            onClick={this.increase}
+            className={btnClass}
+          >
+            increase
+          </button>
+        </div>
+      </>
     );
   }
 }

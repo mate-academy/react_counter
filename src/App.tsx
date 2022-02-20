65f7d6ab6bab1f0926cd2cf120f4d3f60c741b2f
Roma Boreiko <roma.boreiko@gmail.com>
2022-02-20 23:10:33 +0200

    Files tree updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,11 @@
 import React from 'react';
+import './App.scss';
+import { Counter } from './Components/Counter';
 
-type State = {
-  counter: number;
-};
-
-class App extends React.Component<{}, State> {
-  state = {
-    counter: 0,
-  };
-
-  addOne = () => {
-    this.setState((state) => ({
-      counter: state.counter + 1,
-    }));
-  };
-
-  add100 = () => {
-    this.setState((state) => ({
-      counter: state.counter + 100,
-    }));
-  };
-
-  increase = () => {
-    if (this.state.counter % 5 === 0) {
-      this.add100();
-    }
-
-    this.addOne();
-  };
-
+class App extends React.PureComponent {
   render() {
     return (
-      <>
-        <h1>{`Counter - ${this.state.counter}`}</h1>
-        <button
-          type="button"
-          onClick={this.addOne}
-        >
-          Add 1
-        </button>
-        <button
-          type="button"
-          onClick={this.add100}
-        >
-          Add 100
-        </button>
-        <button
-          type="button"
-          onClick={this.increase}
-        >
-          Increase
-        </button>
-      </>
+      <Counter />
     );
   }
 }
---
+++ src/Components/Counter.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+type State = {
+  counter: number,
+};
+
+export class Counter extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
+
+  addOne = () => {
+    this.setState((state) => (
+      {
+        counter: state.counter + 1,
+      }
+    ));
+  };
+
+  add100 = () => {
+    this.setState((state) => (
+      {
+        counter: state.counter + 100,
+      }
+    ));
+  };
+
+  increase = () => {
+    if (this.state.counter % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
+
+  render() {
+    return (
+      <>
+        <h1>{`Counter - ${this.state.counter}`}</h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          Add 1
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+      </>
+    );
+  }
+}

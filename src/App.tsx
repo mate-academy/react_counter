5fa4d4206b4e2371fbf3581bd3cddfa4b422e6fb
krolich500grn <edu.izaytsev@gmail.com>
2022-03-27 17:23:27 +0300

    solution counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,70 @@
 import React from 'react';
+
+import './button.css';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  age: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    age: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      age: state.age + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({
+      age: state.age + 100,
+    }));
+  };
+
+  increase = () => {
+    const expression = this.state.age % 5 === 0
+      ? 101
+      : 1;
+
+    this.setState(state => ({
+      age: state.age + expression,
+    }));
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1 className="App__header">
+          {`Count: ${this.state.age}`}
+        </h1>
+
+        <button
+          type="button"
+          className="button-23"
+          onClick={this.addOne}
+        >
+          Add 1
+        </button>
+
+        <button
+          type="button"
+          className="button-23"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+
+        <button
+          type="button"
+          className="button-23"
+          onClick={this.increase}
+        >
+          Broken button
+        </button>
+      </div>
     );
   }
 }

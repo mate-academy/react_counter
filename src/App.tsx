9ecc49227b9f246a9adeacbb39be7ea6c1263554
Roman Khemych <roman19940912@gmail.com>
2022-03-30 13:53:54 +0300

    Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,72 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type Props = {};
 
-  add100 = () => {};
+interface State {
+  counter: number;
+}
 
-  increase = () => {};
+class App extends React.Component<Props, State> {
+  state: State = {
+    counter: 0,
+  };
+
+  addOne = () => {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      counter: state.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.counter % 5 === 0) {
+      this.add100();
+    }
+  };
+
+  resetCounter = () => this.setState({ counter: 0 });
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1>
+          Count:
+          {' '}
+          { this.state.counter }
+        </h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          Add One
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          Inscrease
+        </button>
+        <button
+          type="button"
+          onClick={this.resetCounter}
+        >
+          Reset
+        </button>
+      </div>
     );
   }
 }

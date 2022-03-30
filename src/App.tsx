ee403d586f971d68a99326033e87e0a0319b12f7
Max Shlikhta <maxok.vibe@gmail.com>
2022-03-30 13:37:04 +0300

    Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,70 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+export class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="app">
+        <div className="app__frame">
+          <h1 className="app__text">
+            {`Count: ${count}`}
+          </h1>
+
+          <div className="app__action">
+            <button
+              type="button"
+              className="app__button"
+              onClick={this.addOne}
+            >
+              Add 1
+            </button>
+            <button
+              type="button"
+              className="app__button"
+              onClick={this.add100}
+            >
+              Add 100
+            </button>
+            <button
+              type="button"
+              className="app__button"
+              onClick={this.increase}
+            >
+              Increase adding...
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
-
-export default App;

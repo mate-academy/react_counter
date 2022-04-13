89b835bfe6afb10f3a52210419839409e528793e
Ivan Major <tarstarcas@gmail.com>
2022-04-13 20:45:50 +0300

    V1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,66 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  amount: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    amount: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      amount: state.amount + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      amount: state.amount + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.amount % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { amount } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          {'Count: '}
+          {amount}
+        </h1>
+
+        <button
+          type="button"
+          onClick={() => this.addOne()}
+        >
+          Add 1
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.add100()}
+        >
+          Add 100
+        </button>
+
+        <button
+          type="button"
+          onClick={() => this.increase()}
+        >
+          Add 1 and 100
+        </button>
+      </>
     );
   }
 }

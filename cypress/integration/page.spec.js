eab1690515acd988f307aea92783d0302aacb85b
murat-yesimbietov <murat.y@mate.academy>
2022-04-19 15:59:55 +0300

    Cypress test have been added

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,7 +1,42 @@
-// WRITE TESTS HERE
-
 describe('Page', () => {
-  it('should be visitable', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it(`should have 'h1' containing '0' on start`, () => {
+    cy.get('h1').should('contain.text', '0');
+  });
+
+  it(`should add 1 when clicking on 'Add 1'`, () => {
+    cy.contains('[type="button"]', 'Add 1')
+      .click();
+
+    cy.get('h1').should('contain.text', '1');
+  });
+
+  it(`should add 100 when clicking on 'Add 100'`, () => {
+    cy.contains('[type="button"]', 'Add 100')
+      .click();
+
+    cy.get('h1').should('contain.text', '100');
+  });
+
+  it(`should add 1 when clicking on 'Increase'`, () => {
+    cy.contains('[type="button"]', 'Increase')
+      .click();
+
+    cy.get('h1').should('contain.text', '1');
+  });
+
+  it(`should additionaly add 100 when counter is divisible by 5 when clicking on 'Increase'`, () => {
+    cy.contains('[type="button"]', 'Add 100')
+      .click();
+
+    cy.get('h1').should('contain.text', '100');
+
+    cy.contains('[type="button"]', 'Increase')
+      .click();
+
+    cy.get('h1').should('contain.text', '201');
   });
 });

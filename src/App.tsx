d8f5dddf62c3e27e58289f3df0bbc45ee90704f8
Olga <arnab@ukr.net>
2022-04-25 17:41:41 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,76 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  currentState: number;
+};
 
-  add100 = () => {};
+export class App extends React.Component<{}, State> {
+  state = {
+    currentState: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      currentState: state.currentState + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      currentState: state.currentState + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState((state) => {
+      if (state.currentState % 5 === 0) {
+        this.add100();
+      }
+
+      return {
+        currentState: state.currentState + 1,
+      };
+    });
+  };
 
   render() {
+    const { currentState } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1 className="App__title">
+          Count:
+          {' '}
+          {currentState}
+        </h1>
+        <div className="App__buttons">
+
+          <button
+            type="button"
+            className="App__button"
+            onClick={this.addOne}
+          >
+            Add 1
+          </button>
+
+          <button
+            type="button"
+            className="App__button"
+            onClick={this.add100}
+          >
+            Add 100
+          </button>
+
+          <button
+            type="button"
+            className="App__button"
+            onClick={this.increase}
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     );
   }
 }
-
-export default App;

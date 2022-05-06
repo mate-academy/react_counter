6ceb3ca8c6a100491d42412b7fd67ae5f8d277e1
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-05-06 17:05:55 +0300

    Counter was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    this.addOne();
+
+    if (count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1 className="App__title">{`Count: ${this.state.count}`}</h1>
+        <button type="button" className="App__button" onClick={this.addOne}>
+          Add 1
+        </button>
+
+        <button type="button" className="App__button" onClick={this.add100}>
+          Add 100
+        </button>
+
+        <button type="button" className="App__button" onClick={this.increase}>
+          Add 1 or more
+        </button>
+      </div>
     );
   }
 }

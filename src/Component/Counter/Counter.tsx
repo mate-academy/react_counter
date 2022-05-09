8028e9d70d80f68d0eaca18785cb1a665c786a86
SeKaM0 <polozhentsew02@gmail.com>
2022-05-09 19:42:19 +0300

    Fix mistakes

---
+++ src/Component/Counter/Counter.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './Counter.scss';
+
+type State = {
+  count: number,
+};
+
+export class Counter extends React.Component<{}, State> {
+  state:State = {
+    count: 0,
+  };
+
+  addOne = () => {
+    this.setState((state) => ({ count: state.count + 1 }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({ count: state.count + 100 }));
+  };
+
+  increase = () => {
+    this.addOne();
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <>
+        <h1>
+          Count:
+          {count}
+        </h1>
+
+        <button
+          className="button"
+          type="button"
+          onClick={this.addOne}
+        >
+          add 1
+        </button>
+        <button
+          className="button"
+          type="button"
+          onClick={this.add100}
+        >
+          add 100
+        </button>
+        <button
+          className="button"
+          type="button"
+          onClick={this.increase}
+        >
+          increase
+        </button>
+      </>
+    );
+  }
+}

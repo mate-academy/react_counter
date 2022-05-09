99c9943293b7400d720eaae6e1b3b73941610054
Max Nikiforov <maxxnikiforov@gmail.com>
2022-05-10 00:59:05 +0700

    easy solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,65 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+export class App extends React.Component<{}, State> {
+  state: State = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="app">
+        <h1 className="app__title">
+          Count:&nbsp;
+          {this.state.count}
+        </h1>
+        <button
+          className="app__button"
+          type="button"
+          onClick={this.addOne}
+        >
+          Add one
+        </button>
+
+        <button
+          className="app__button"
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+
+        <button
+          className="app__button"
+          type="button"
+          onClick={this.increase}
+        >
+          Strange button
+        </button>
+      </div>
     );
   }
 }
-
-export default App;

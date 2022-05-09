483eb3e4767adecf439662c2d3b586e03ec43039
SeKaM0 <polozhentsew02@gmail.com>
2022-05-09 19:41:54 +0300

    Fix mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,12 @@
 import React from 'react';
 import './App.scss';
+import { Counter } from './Component/Counter/Counter';
 
-type State = {
-  count: number,
-};
-class App extends React.Component<{}, State> {
-  state:State = {
-    count: 0,
-  };
-
-  addOne = () => {
-    this.setState((state) => ({ count: state.count + 1 }));
-  };
-
-  add100 = () => {
-    this.setState((state) => ({ count: state.count + 100 }));
-  };
-
-  increase = () => {
-    this.addOne();
-    if (this.state.count % 5 === 0) {
-      this.add100();
-    }
-  };
-
-  render() {
-    const { count } = this.state;
-
+class App extends React.PureComponent {
+  render(): React.ReactNode {
     return (
       <div className="App">
-        <h1>
-          Count:
-          {count}
-        </h1>
-
-        <button
-          className="button"
-          type="button"
-          onClick={this.addOne}
-        >
-          add 1
-        </button>
-        {' '}
-        <button
-          className="button"
-          type="button"
-          onClick={this.add100}
-        >
-          add 100
-        </button>
-        {' '}
-        <button
-          className="button"
-          type="button"
-          onClick={this.increase}
-        >
-          increase
-        </button>
+        <Counter />
       </div>
     );
   }

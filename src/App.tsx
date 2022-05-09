16d47a2b5dc6a27f7e843d6449fa077277a6d02c
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-05-09 06:21:56 +0300

    react-counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type Props = {};
 
-  add100 = () => {};
+type State = {
+  count: number
+};
 
-  increase = () => {};
+class App extends React.Component<Props, State> {
+  state = {
+    count: 0,
+  };
+
+  addOne = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({ count: state.count + 100 }));
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    this.addOne();
+
+    if (count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div>
+        <h1>{`Count: ${count}`}</h1>
+        <button type="button" onClick={this.addOne}>Add One</button>
+        <button type="button" onClick={this.add100}>Add Hundred</button>
+        <button type="button" onClick={this.increase}>Increase</button>
+      </div>
     );
   }
 }

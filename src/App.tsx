6ddf16540081b22b7e9a5684482a3ae733905e78
Nikita Prepelytsia <prep.nikita@gmail.com>
2022-05-10 07:01:10 +0300

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,73 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => (
+      { counter: prevState.counter + 1 }
+    ));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => (
+      { counter: prevState.counter + 100 }
+    ));
+  };
+
+  increase = () => {
+    this.setState((prevState) => {
+      if (prevState.counter % 5 === 0) {
+        this.add100();
+      }
+
+      this.addOne();
+    });
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="app">
+        <h1 className="app__counter">
+          Count:
+          {' '}
+          {this.state.counter}
+        </h1>
+
+        <div className="app__buttons">
+          <button
+            type="button"
+            onClick={this.addOne}
+            className="app__button"
+          >
+            Add 1
+          </button>
+
+          <button
+            type="button"
+            onClick={this.add100}
+            className="app__button"
+          >
+            Add 100
+          </button>
+
+          <button
+            type="button"
+            onClick={this.increase}
+            className="app__button"
+          >
+            Add some magic
+          </button>
+        </div>
+      </div>
+
     );
   }
 }

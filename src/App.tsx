1ba678ddf086dda6891f50903906dbb48ded0c6d
Misha Kravtsov <aniahucm@gmail.com>
2022-05-10 11:07:22 +0300

    implement count

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,56 @@
 import React from 'react';
-import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  incrementCount = (num: number) => {
+    this.setState((prevState) => ({ count: prevState.count + num }));
+  };
+
+  addOne = () => {
+    this.incrementCount(1);
+  };
+
+  add100 = () => {
+    this.incrementCount(100);
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.incrementCount(100);
+    }
+
+    this.incrementCount(1);
+  };
 
   render() {
+    const { count } = this.state;
+
+    const btnIncrement1 = React.createElement('button',
+      { type: 'button', onClick: this.addOne }, 'addOne');
+
+    const btnIncrement2 = React.createElement('button',
+      { type: 'button', onClick: this.add100 }, 'add100');
+
+    const btnIncrement3 = React.createElement('button',
+      { type: 'button', onClick: this.increase }, 'increase');
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {count}
+        </h1>
+        {btnIncrement1}
+        {btnIncrement2}
+        {btnIncrement3}
+      </>
     );
   }
 }

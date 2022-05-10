7c7045dd35df6e7f7e73f58a919bf4f23a0dd544
Helen Petrovska <paznachka@gmail.com>
2022-05-10 14:22:20 +0300

    my counter was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,73 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+interface State {
+  counter: number;
+}
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 100,
+    }));
+  };
+
+  increase = (prevState: State) => {
+    if (prevState.counter % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { counter } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="counter">
+        <h1 className="counter__title">
+          Count:
+          {' '}
+          {counter}
+        </h1>
+        <div className="counter__buttons">
+          <button
+            type="button"
+            className="button"
+            onClick={() => this.addOne()}
+          >
+            One
+          </button>
+
+          <button
+            type="button"
+            className="button"
+            onClick={() => this.add100()}
+          >
+            100
+          </button>
+
+          <button
+            type="button"
+            className="button"
+            onClick={() => {
+              this.increase(this.state);
+            }}
+          >
+            increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

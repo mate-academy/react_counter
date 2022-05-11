2551cd01855eecd0c82d6362dd973148151c1baf
Andriy Shvets <PiratesQ@i.ua>
2022-05-11 10:00:10 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,82 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type Props = {};
 
-  add100 = () => {};
+type State = {
+  counter: number,
+};
 
-  increase = () => {};
+class App extends React.Component<Props, State> {
+  state = {
+    counter: 0,
+  };
+
+  addOne = () => {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      counter: state.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.counter % 5 === 0) {
+      this.add100();
+    } else {
+      this.addOne();
+    }
+  };
+
+  reset = () => {
+    this.setState({ counter: 0 });
+  };
 
   render() {
+    const { counter } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div>
+        <h1>
+          Count:
+          {` ${counter}`}
+        </h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+          className="button"
+        >
+          One
+        </button>
+
+        <button
+          type="button"
+          onClick={this.add100}
+          className="button"
+        >
+          Two
+        </button>
+
+        <button
+          type="button"
+          onClick={this.increase}
+          className="button"
+        >
+          Three
+        </button>
+
+        <button
+          type="button"
+          onClick={this.reset}
+          className="button"
+        >
+          Reset
+        </button>
+      </div>
     );
   }
 }

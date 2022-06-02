d51dd1846abd1f46f4d5a4a35d68b9de49173e5d
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-06-02 20:15:30 +0300

    Fixes after review2

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -32,32 +32,30 @@ export class Counter extends React.Component <{}, State> {
 
   render() {
     return (
-      <>
-        <div className="block box level level-item title is-4">
-          <h1>{`Count: ${this.state.count}`}</h1>
-          <button
-            type="button"
-            onClick={this.addOne}
-            className="button is-primary m-3 title is-4"
-          >
-            Add 1
-          </button>
-          <button
-            type="button"
-            onClick={this.add100}
-            className="button is-primary m-3 title is-4"
-          >
-            Add 100
-          </button>
-          <button
-            type="button"
-            onClick={this.increase}
-            className="button is-primary m-3 title is-4"
-          >
-            Increase
-          </button>
-        </div>
-      </>
+      <div className="block box level level-item title is-4">
+        <h1>{`Count: ${this.state.count}`}</h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+          className="button is-primary m-3 title is-4"
+        >
+          Add 1
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+          className="button is-primary m-3 title is-4"
+        >
+          Add 100
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+          className="button is-primary m-3 title is-4"
+        >
+          Increase
+        </button>
+      </div>
     );
   }
 }

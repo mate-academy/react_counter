8ebae05bfcffb3a9d83a3576560938a038844992
Daria Liubymova <dellachaise@gmail.com>
2022-06-03 00:55:47 +0300

    Solved counter task

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,74 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number,
+  increaseCounter: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+    increaseCounter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState(state => ({
+      counter: state.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState(state => ({
+      counter: state.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.increaseCounter === 0 || this.state.increaseCounter % 5) {
+      this.setState(state => ({
+        counter: state.counter + 1,
+        increaseCounter: state.increaseCounter + 1,
+      }));
+    } else {
+      this.setState(state => ({
+        counter: state.counter + 101,
+        increaseCounter: state.increaseCounter + 1,
+      }));
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <div className="App__counter">
+          <h1 className="App__title">
+            Count:
+            {this.state.counter}
+          </h1>
+          <button
+            type="button"
+            onClick={this.addOne}
+            className="button is-large is-responsive is-rounded"
+          >
+            Add 1
+          </button>
+          <button
+            type="button"
+            onClick={this.add100}
+            className="button is-large is-responsive is-rounded"
+          >
+            Add 100
+          </button>
+          <button
+            type="button"
+            onClick={this.increase}
+            className="button is-large is-responsive is-rounded"
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

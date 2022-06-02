89e4d1caf76b66c12ee40c3ddd8308c7d9cb34ce
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-06-02 20:13:35 +0300

    Fixes after review

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom';
-import { Counter } from './Counter';
+import { App } from './App';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

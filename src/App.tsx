43a0692641c9883faf85d4c9c715c49a0332a13f
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-06-02 22:59:40 +0200

    made new counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,72 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type Props = {};
 
-  add100 = () => {};
+type State = {
+  count: number
+};
 
-  increase = () => {};
+class App extends React.Component<Props, State> {
+  state: State = {
+    count: 0,
+  };
+
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="container">
+        <h1 className="container__title">
+          Count:
+          {' '}
+          {count}
+        </h1>
+        <br />
+        <div className="container__items">
+          <button
+            type="button"
+            onClick={this.addOne}
+            className="button button__one"
+          >
+            Add 1
+          </button>
+          <button
+            type="button"
+            onClick={this.add100}
+            className="button button__100"
+          >
+            Add 100
+          </button>
+          <button
+            type="button"
+            onClick={this.increase}
+            className="button button__increase"
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

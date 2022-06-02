b1854fe2ce13b1cd205ae2a08bf2d9bfbdabc797
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-06-02 11:46:41 +0300

    Counter was done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,63 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+export class App extends React.Component <{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <>
+        <div className="block box level level-item title is-4">
+          <h1>{`Count: ${this.state.count}`}</h1>
+          <button
+            type="button"
+            onClick={this.addOne}
+            className="button is-primary m-3 title is-4"
+          >
+            Add 1
+          </button>
+          <button
+            type="button"
+            onClick={this.add100}
+            className="button is-primary m-3 title is-4"
+          >
+            Add 100
+          </button>
+          <button
+            type="button"
+            onClick={this.increase}
+            className="button is-primary m-3 title is-4"
+          >
+            Increase
+          </button>
+        </div>
+      </>
     );
   }
 }
-
-export default App;

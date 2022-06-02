021de64872710b5c2d11bf04c76bc429b96f7621
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-06-02 16:51:28 +0300

    Counter component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,18 @@
 import React from 'react';
 import './App.scss';
 
-type State = {
-  count: number;
-};
+class App extends React.Component {
+  addOne = () => {};
 
-export class App extends React.Component <{}, State> {
-  state = {
-    count: 0,
-  };
+  add100 = () => {};
 
-  addOne = () => {
-    this.setState((state) => ({
-      count: state.count + 1,
-    }));
-  };
-
-  add100 = () => {
-    this.setState((state) => ({
-      count: state.count + 100,
-    }));
-  };
-
-  increase = () => {
-    this.addOne();
-
-    if (this.state.count % 5 === 0) {
-      this.add100();
-    }
-  };
+  increase = () => {};
 
   render() {
     return (
-      <>
-        <div className="block box level level-item title is-4">
-          <h1>{`Count: ${this.state.count}`}</h1>
-          <button
-            type="button"
-            onClick={this.addOne}
-            className="button is-primary m-3 title is-4"
-          >
-            Add 1
-          </button>
-          <button
-            type="button"
-            onClick={this.add100}
-            className="button is-primary m-3 title is-4"
-          >
-            Add 100
-          </button>
-          <button
-            type="button"
-            onClick={this.increase}
-            className="button is-primary m-3 title is-4"
-          >
-            Increase
-          </button>
-        </div>
-      </>
+      <h1>Count: 0</h1>
     );
   }
 }
+
+export default App;

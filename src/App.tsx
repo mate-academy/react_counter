d0943572cede3d0554fdfcbc6d0ae9c4457b4a8b
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2022-06-03 01:27:50 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,60 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+interface State {
+  x: number
+}
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    x: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => ({ x: prevState.x + 1 }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({ x: prevState.x + 100 }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.x % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { x } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="content has-text-centered mt-5">
+        <h1>{`Count: ${x}`}</h1>
+        <button
+          className="button is-success is-outlined mr-5"
+          type="button"
+          onClick={this.addOne}
+        >
+          Add 1
+        </button>
+
+        <button
+          className="button is-warning is-outlined mr-5"
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+        <button
+          className="button is-danger is-outlined"
+          type="button"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+      </div>
     );
   }
 }

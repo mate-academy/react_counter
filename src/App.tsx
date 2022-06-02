15bfd312724ae4ba1d9e9287fb1db6dc1b7c13f9
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-06-02 20:30:46 +0300

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,79 @@
 import React from 'react';
 import './App.scss';
+import { State } from './react-app-env';
 
-class App extends React.Component {
-  addOne = () => {};
+export class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+    }));
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState((state) => ({
+      counter: state.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState((state) => {
+      if (state.counter % 5 === 0) {
+        this.addOne();
+        this.add100();
+      } else {
+        this.addOne();
+      }
+    });
+  };
+
+  setClear = () => {
+    this.setState({ counter: 0 });
+  };
 
   render() {
+    const { counter } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="counter content is-medium">
+        <h1 className="counter__title">
+          Count:
+          {counter}
+        </h1>
+        <div className="counter__button-box">
+          <button
+            type="button"
+            onClick={this.addOne}
+            className="counter__button__one button is-info is-rounded"
+          >
+            Add one
+          </button>
+          <button
+            type="button"
+            className="counter__button__hundred button is-info is-rounded"
+            onClick={this.add100}
+          >
+            Add one hundred
+          </button>
+          <button
+            type="button"
+            className="counter__button__increase button is-warning is-rounded"
+            onClick={this.increase}
+          >
+            Increase
+          </button>
+        </div>
+        <button
+          type="button"
+          className="counter__button__clear button is-danger is-rounded"
+          onClick={this.setClear}
+        >
+          Clear
+        </button>
+      </div>
     );
   }
 }
-
-export default App;

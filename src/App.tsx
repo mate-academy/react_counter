712c4c79fb5d84a36d33efec76c96588076a15d3
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-06-03 18:54:40 +0300

    count

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,69 @@
+import 'bulma/css/bulma.min.css';
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="container">
+        <h1 className="title is-1">
+          {`Count: ${count}`}
+        </h1>
+
+        <button
+          type="button"
+          onClick={this.addOne}
+          className="button is-primary is-rounded"
+        >
+          One
+        </button>
+
+        <button
+          type="button"
+          onClick={this.add100}
+          className="button is-primary is-rounded"
+        >
+          OneHundred
+        </button>
+
+        <button
+          type="button"
+          onClick={this.increase}
+          className="button is-primary is-rounded"
+        >
+          Increase
+        </button>
+      </div>
     );
   }
 }

d732c0138b0af512e60e285831a5c744b66ce724
Daria Liubymova <dellachaise@gmail.com>
2022-06-03 12:57:53 +0300

    Moved counter to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,11 @@
 import React from 'react';
 import './App.scss';
+import { Counter } from './components/Counter/Counter';
 
-type State = {
-  counter: number,
-};
-
-class App extends React.Component<{}, State> {
-  state = {
-    counter: 0,
-  };
-
-  addOne = () => {
-    this.setState(state => ({
-      counter: state.counter + 1,
-    }));
-  };
-
-  add100 = () => {
-    this.setState(state => ({
-      counter: state.counter + 100,
-    }));
-  };
-
-  increase = () => {
-    this.addOne();
-
-    if (!(this.state.counter % 5)) {
-      this.add100();
-    }
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App__counter">
-          <h1 className="App__title">
-            Count:
-            {this.state.counter}
-          </h1>
-          <button
-            type="button"
-            onClick={this.addOne}
-            className="button is-large is-responsive is-rounded"
-          >
-            Add 1
-          </button>
-          <button
-            type="button"
-            onClick={this.add100}
-            className="button is-large is-responsive is-rounded"
-          >
-            Add 100
-          </button>
-          <button
-            type="button"
-            onClick={this.increase}
-            className="button is-large is-responsive is-rounded"
-          >
-            Increase
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+const App: React.FC = () => (
+  <div className="App">
+    <Counter />
+  </div>
+);
 
 export default App;

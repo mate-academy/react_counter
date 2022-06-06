67482e47d7e2500371fd799bd8ac2f92e0a82b9a
Ihor Yeremenko <igoree86@gmail.com>
2022-06-06 09:20:28 +0300

    Commit_without_CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,80 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  sum: number,
+};
 
-  add100 = () => {};
+type Props = {
 
-  increase = () => {};
+};
+
+class App extends React.Component<Props, State> {
+  state: State = {
+    sum: 0,
+  };
+
+  addOne = () => {
+    this.setState((state: State) => ({
+      sum: state.sum + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state: State) => ({
+      sum: state.sum + 100,
+    }));
+  };
+
+  increase = () => (
+    this.state.sum % 5 ? (
+      this.addOne()
+    ) : (
+      this.add100()
+    )
+  );
 
   render() {
+    const { sum } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div>
+        <h1>
+          Count:
+          {sum}
+        </h1>
+
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              this.addOne();
+            }}
+          >
+            Add 1
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              this.add100();
+            }}
+          >
+            Add 100
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              this.increase();
+            }}
+          >
+            Increase
+          </button>
+
+        </div>
+      </div>
+
     );
   }
 }

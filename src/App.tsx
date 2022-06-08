6410c854729d54157750c3c3ec1cdfc27f268d78
Oleksandr Strilchuk <virass.m@gmail.com>
2022-06-08 19:51:29 +0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,65 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => {
+      return { count: prevState.count + 1 };
+    });
+  };
+
+  add100 = () => {
+    this.setState((prevState) => {
+      return { count: prevState.count + 100 };
+    });
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    if (count % 5 === 0 && count > 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          Count:
+          {` ${count}`}
+        </h1>
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          Add 1
+        </button>
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+      </>
     );
   }
 }

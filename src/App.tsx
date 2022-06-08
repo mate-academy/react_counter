f4c78324e8f4dc20126e705e3c296430ece92b03
Andrei Sucman <sucmanandrei@gmail.com>
2022-06-08 18:21:02 +0300

    task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,63 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number;
+};
 
-  add100 = () => {};
+export class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state: { counter: number; }) => ({
+      counter: state.counter + 1,
+    }));
+  };
 
-  render() {
+  add100 = () => {
+    this.setState((state: { counter: number }) => ({
+      counter: state.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    this.addOne();
+
+    if (this.state.counter % 5 === 0) {
+      this.add100();
+    }
+  };
+
+  render(): React.ReactNode {
+    const { counter } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>{`Count: ${counter}`}</h1>
+
+        <button
+          type="button"
+          onClick={this.addOne}
+        >
+          Add 1
+        </button>
+
+        <button
+          type="button"
+          onClick={this.add100}
+        >
+          Add 100
+        </button>
+
+        <button
+          type="button"
+          onClick={this.increase}
+        >
+          Increase
+        </button>
+      </>
     );
   }
 }

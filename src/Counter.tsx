f5d25e56db75b2cbc01f10d9dcf4d361e938b3f0
Nazar Muliar <nazarmuliar26@gmail.com>
2022-06-15 17:33:43 +0300

    practiced classnames library

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Counter.scss';
+import classNames from 'classnames';
 
 type Props = {
   count: number;
@@ -11,26 +12,6 @@ type Props = {
 export const Counter: React.FC<Props> = ({
   count, addOne, add100, increase,
 }) => {
-  const getCountClasses = () => {
-    if (count >= 2000) {
-      return 'has-text-link is-size-1 has-background-success';
-    }
-
-    if (count >= 1500) {
-      return 'has-text-link is-size-2 has-background-primary';
-    }
-
-    if (count >= 1000) {
-      return 'has-background-info is-size-3';
-    }
-
-    if (count >= 500) {
-      return 'has-background-warning is-size-4';
-    }
-
-    return 'has-background-danger is-size-4';
-  };
-
   const getKopiykasCase = (number: number) => {
     const lastChar = String(number).slice(-1);
     const twoLastChars = String(number).slice(-2);
@@ -53,9 +34,17 @@ export const Counter: React.FC<Props> = ({
         <h1 className="title level-left">
           How much should this project cost on freelance?
         </h1>
-        <p className={`
-          has-text-centered
-          ${getCountClasses()}`}
+        <p className={classNames(
+          'has-text-centered', {
+            'is-size-1 has-background-success': count >= 2000,
+            'is-size-2 has-background-primary': count < 2000 && count >= 1500,
+            'has-text-link': count >= 1500,
+            'has-background-info is-size-3': count < 1500 && count >= 1000,
+            'is-size-4': count < 1000,
+            'has-background-warning': count >= 500 && count < 1000,
+            'has-background-danger': count < 500,
+          },
+        )}
         >
           {`${count} ${getKopiykasCase(count)}`}
         </p>
@@ -83,11 +72,12 @@ export const Counter: React.FC<Props> = ({
         </button>
         <button
           type="button"
-          className={`
-            button
-            is-medium
-            ${count % 5 === 0 ? 'is-success' : 'is-info'}
-          `}
+          className={classNames(
+            'button is-medium', {
+              'is-success': count % 5 === 0,
+              'is-info': count % 5 !== 0,
+            },
+          )}
           onClick={() => increase()}
         >
           Increase

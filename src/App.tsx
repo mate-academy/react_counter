db24adc18b095f9e2c9c3a5963bbe4d5f7f04104
Andrii Nychaiuk <anichajuk@gmail.com>
2022-07-10 12:42:31 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,63 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+interface State {
+  counter: number;
+}
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((currentState) => ({
+      counter: currentState.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((currentState) => ({
+      counter: currentState.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.counter % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App has-text-centered is-vcentered">
+        <h1 className="title">{`Count: ${this.state.counter}`}</h1>
+        <div className="buttons is-centered">
+          <button
+            type="button"
+            className="button is-primary is-light box is-rounded"
+            onClick={this.addOne}
+          >
+            Add 1
+          </button>
+          <button
+            type="button"
+            className="button is-primary is-light box is-rounded"
+            onClick={this.add100}
+          >
+            Add 100
+          </button>
+          <button
+            type="button"
+            className="button is-primary is-light box is-rounded"
+            onClick={this.increase}
+          >
+            Add 1 (and 100)
+          </button>
+        </div>
+      </div>
     );
   }
 }

e3c8fd578d666cdb83d60500a9dfdf143f329a71
Ostap Salo <jlumon211@gmail.com>
2022-07-12 11:18:34 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,101 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+const animateButton = (e:any) => {
+  e.preventDefault();
 
-  add100 = () => {};
+  e.target.classList.remove('animate');
 
-  increase = () => {};
+  e.target.classList.add('animate');
+  setTimeout(() => {
+    e.target.classList.remove('animate');
+  }, 700);
+};
+
+const bubblyButtons = document.getElementsByClassName('bubbly-button');
+
+// eslint-disable-next-line no-plusplus
+for (let i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
+type State = {
+  age: number,
+  count: number,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    age: 0,
+    count: 0,
+  };
+
+  addOne = () => {
+    this.setState((prevState) => ({
+      age: prevState.age + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prevState) => ({
+      age: prevState.age + 100,
+    }));
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    if (count < 5) {
+      this.setState((prevState) => ({
+        age: prevState.age + 1,
+        count: prevState.count + 1,
+      }));
+    } else {
+      this.setState((prevState) => ({
+        age: prevState.age + 100,
+        count: prevState.count + 1,
+      }));
+    }
+  };
 
   render() {
+    const { age } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1
+          className="App__title"
+        >
+          {`Current age: ${age}`}
+        </h1>
+
+        <div className="App__button-container">
+          <button
+            onClick={this.addOne}
+            type="button"
+            className="App__button"
+          >
+            Add1
+          </button>
+
+          <button
+            onClick={this.add100}
+            type="button"
+            className="App__button"
+          >
+            Add100
+          </button>
+
+          <button
+            onClick={this.increase}
+            type="button"
+            className="App__button"
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

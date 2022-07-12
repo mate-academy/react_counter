241162b892fb84dcbce6e8dc700d374aea120b94
Roman Marunkevich <vinnibea@gmail.com>
2022-07-12 09:58:23 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type Counter = {
+  result: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, Counter> {
+  public state = {
+    result: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((currentState) => ({
+      result: currentState.result + 1,
+    }));
+
+    return 0;
+  };
+
+  add100 = () => {
+    this.setState((currentState) => ({
+      result: currentState.result + 100,
+    }));
+
+    return 0;
+  };
+
+  increase = () => {
+    const { result } = this.state;
+
+    if (result % 5 === 0) {
+      return (this.add100() + this.addOne());
+    }
+
+    return this.addOne();
+  };
 
   render() {
+    const { result } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>
+          {`Count: ${result}`}
+        </h1>
+        <button type="button" onClick={this.addOne}> Add 1 </button>
+        <button type="button" onClick={this.add100}> Add 100 </button>
+        <button type="button" onClick={this.increase}> Icrease </button>
+      </>
     );
   }
 }

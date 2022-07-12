1facd0e7d751dd46cfec4f2189bb3573a69b128c
Sasha Havryliuk <princesasha9178@icloud.com>
2022-07-12 20:52:49 +0300

    Solution(without Bulma)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,69 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prev) => ({
+      counter: prev.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((prev) => ({
+      counter: prev.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.counter % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { counter } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <h1 className="paragraph">
+          Count:
+          {' '}
+          {counter}
+        </h1>
+        <div className="buttons">
+          <button
+            className="btn"
+            type="button"
+            onClick={this.addOne}
+          >
+            Add 1
+          </button>
+          <button
+            className="btn"
+            type="button"
+            onClick={this.add100}
+          >
+            Add 100
+          </button>
+          <button
+            className="btn"
+            type="button"
+            onClick={this.increase}
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

6fa5e08e0599ce3473dee7d228b4f0fceae39829
Olya Yevsieienko <olya.evsnko@gmail.com>
2022-07-12 18:40:03 +0300

    Created counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,69 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    if (count % 5 === 0) {
+      this.add100();
+      this.addOne();
+    } else {
+      this.addOne();
+    }
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <div className="App__container">
+          <h1 className="App__title">
+            {`Count: ${this.state.count}`}
+          </h1>
+
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.addOne}
+          >
+            Add 1
+          </button>
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.add100}
+          >
+            Add 100
+          </button>
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.increase}
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

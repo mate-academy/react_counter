006a785a064fde3ddfb069f025fa8636ac3425e3
Bogdan Pyshchyk <bohdan.pyshchyk@gmail.com>
2022-07-13 09:00:49 +0300

    Counter 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,72 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  number: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    number: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((prevState) => {
+      return {
+        number: prevState.number + 1,
+      };
+    });
+  };
+
+  add100 = () => {
+    this.setState((prevState) => {
+      return {
+        number: prevState.number + 100,
+      };
+    });
+  };
+
+  increase = () => {
+    const { number } = this.state;
+
+    if (number % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { number } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="container">
+        <h1 className="title">{`Count: ${number}`}</h1>
+
+        <button
+          type="button"
+          onClick={this.addOne}
+          className="button"
+        >
+          Add 1
+        </button>
+
+        <button
+          type="button"
+          onClick={this.add100}
+          className="button"
+        >
+          Add 100
+        </button>
+
+        <button
+          type="button"
+          onClick={this.increase}
+          className="button"
+        >
+          Increase
+        </button>
+      </div>
     );
   }
 }

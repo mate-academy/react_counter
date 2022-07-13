2e4dedd544b321e4ad882476849736992f8a943a
Ivan Grabovsky <ivan.grabovsky.ua@gmail.com>
2022-07-13 08:01:45 +0300

    first commit - add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,86 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      counter: state.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState((state) => {
+      if (state.counter % 5 === 0 && state.counter !== 0) {
+        this.add100();
+      }
+
+      this.addOne();
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      counter: 0,
+    });
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="box has-text-centered mt-6">
+        <h1 className="title is-size-3">{`Count: ${this.state.counter}`}</h1>
+        <div className="columns">
+          <div className="column">
+            <button
+              type="button"
+              className="button is-responsive"
+              onClick={this.addOne}
+            >
+              Add 1
+            </button>
+          </div>
+          <div className="column">
+            <button
+              type="button"
+              className="button is-responsive"
+              onClick={this.add100}
+            >
+              Add 100
+            </button>
+          </div>
+          <div className="column">
+            <button
+              type="button"
+              className="button is-responsive"
+              onClick={this.increase}
+            >
+              Increace
+            </button>
+          </div>
+          <div className="column">
+            <button
+              type="button"
+              className="button is-responsive"
+              onClick={this.reset}
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }

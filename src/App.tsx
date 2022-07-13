5e1caeebe72760335327ef96a76376e6e3889bd9
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-07-13 12:41:30 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,105 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  counter: number,
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    counter: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((currentState) => ({
+      counter: currentState.counter + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((currentState) => ({
+      counter: currentState.counter + 100,
+    }));
+  };
+
+  increase = () => {
+    this.setState((currentState) => {
+      if (currentState.counter % 5 === 0 && currentState.counter > 0) {
+        this.addOne();
+        this.add100();
+      } else {
+        this.addOne();
+      }
+    });
+  };
+
+  clear = () => {
+    this.setState({ counter: 0 });
+  };
 
   render() {
     return (
-      <h1>Count: 0</h1>
+      <div className="App">
+        <div className="container">
+          <h1 className="title ml-5">
+            {`Count: ${this.state.counter}`}
+          </h1>
+          <div className="level">
+            <button
+              className="
+              button
+              is-info
+              is-outlined
+              ml-5
+              level-item
+              "
+              type="button"
+              onClick={this.addOne}
+            >
+              Add 1
+            </button>
+            <button
+              className="
+              button
+              is-info
+              is-outlined
+              ml-5
+              level-item
+              "
+              type="button"
+              onClick={this.add100}
+            >
+              Add 100
+            </button>
+            <button
+              className="
+              button
+              is-info
+              is-outlined
+              ml-5
+              level-item
+              "
+              type="button"
+              onClick={this.increase}
+            >
+              Increase
+            </button>
+            <button
+              className="
+              button
+              is-danger
+              is-outlined
+              ml-5
+              level-item
+              "
+              type="button"
+              onClick={this.clear}
+            >
+              Clear
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }

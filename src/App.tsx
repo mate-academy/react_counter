975fc18dc5a6753001241cabe08157981c7a1fea
Denys Politov <dpolitov2001@gmail.com>
2022-07-13 16:37:39 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,62 @@
 import React from 'react';
 import './App.scss';
 
-class App extends React.Component {
-  addOne = () => {};
+type State = {
+  count: number;
+};
 
-  add100 = () => {};
+class App extends React.Component<{}, State> {
+  state = {
+    count: 0,
+  };
 
-  increase = () => {};
+  addOne = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  add100 = () => {
+    this.setState((state) => ({
+      count: state.count + 100,
+    }));
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
+    const { addOne, add100, increase } = this;
+
     return (
-      <h1>Count: 0</h1>
+      <>
+        <h1>{`Count: ${count}`}</h1>
+        <button
+          type="button"
+          onClick={addOne}
+        >
+          Add 1
+        </button>
+        <button
+          type="button"
+          onClick={add100}
+        >
+          Add 100
+        </button>
+        <button
+          type="button"
+          onClick={increase}
+        >
+          Increase
+        </button>
+      </>
     );
   }
 }

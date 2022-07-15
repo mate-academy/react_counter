5315344edddbfcbe4ec3840b40f5478197f8f79f
Dima <d.maik2178@gmail.com>
2022-07-15 17:14:13 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,75 @@
 import React from 'react';
 import './App.scss';
 
+interface State {
+  count: number;
+}
+
 class App extends React.Component {
-  addOne = () => {};
+  state: State = {
+    count: 0,
+  };
 
-  add100 = () => {};
+  addOne = () => {
+    this.setState((prev: State) => {
+      return {
+        count: prev.count + 1,
+      };
+    });
+  };
 
-  increase = () => {};
+  add100 = () => {
+    this.setState((prev: State) => {
+      return {
+        count: prev.count + 100,
+      };
+    });
+  };
+
+  increase = () => {
+    if (this.state.count % 5 === 0) {
+      this.add100();
+    }
+
+    this.addOne();
+  };
 
   render() {
+    const { count } = this.state;
+
     return (
-      <h1>Count: 0</h1>
+      <div className="container">
+        <h1>{count}</h1>
+
+        <div className="button__block">
+          <button
+            onClick={() => {
+              this.addOne();
+            }}
+            type="button"
+          >
+            Add 1
+          </button>
+
+          <button
+            onClick={() => {
+              this.add100();
+            }}
+            type="button"
+          >
+            Add 100
+          </button>
+
+          <button
+            onClick={() => {
+              this.increase();
+            }}
+            type="button"
+          >
+            Increase
+          </button>
+        </div>
+      </div>
     );
   }
 }

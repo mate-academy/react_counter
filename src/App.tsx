d4d76429a626e68c12950e50b7160c240d668cdf
Tania Troshchuk <troschuk95@gmail.com>
2022-07-23 12:05:29 +0300

    Solution Task React counter

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,18 @@ export class App extends React.Component<{}, State> {
     count: 0,
   };
 
-  // Fix addOne and add100 methods so they can work together
   addOne = () => {
-    let { count } = this.state;
-
-    count += 1;
-
-    this.setState({ count });
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
   };
 
   add100 = () => {
-    let { count } = this.state;
-
-    count += 100;
-
-    this.setState({ count });
+    this.setState(state => ({
+      count: state.count + 100,
+    }));
   };
 
-  // DON'T change the code below
   increase = () => {
     const { count } = this.state;
 
@@ -47,15 +41,27 @@ export class App extends React.Component<{}, State> {
           {`Count: ${count}`}
         </h1>
 
-        <button type="button" className="App__add-one" onClick={this.addOne}>
+        <button
+          type="button"
+          className="App__add-one"
+          onClick={this.addOne}
+        >
           Add 1
         </button>
 
-        <button type="button" className="App__add-100" onClick={this.add100}>
+        <button
+          type="button"
+          className="App__add-100"
+          onClick={this.add100}
+        >
           Add 100
         </button>
 
-        <button type="button" className="App__increase" onClick={this.increase}>
+        <button
+          type="button"
+          className="App__increase"
+          onClick={this.increase}
+        >
           Increase
         </button>
       </div>

91a7face77810f638764041350dbbf966d6054e2
Mariya Sukhanova <suhanovamasha21@gmail.com>
2022-08-09 22:17:41 +0300

    Added solution

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,19 @@ export class App extends React.Component<{}, State> {
 
   // Fix addOne and add100 methods so they can work together
   addOne = () => {
-    let { count } = this.state;
-
-    count += 1;
-
-    this.setState({ count });
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
   };
 
   add100 = () => {
-    let { count } = this.state;
-
-    count += 100;
-
-    this.setState({ count });
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 100,
+      };
+    });
   };
 
   // DON'T change the code below

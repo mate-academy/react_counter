bcef0d6a255df78868232c08c5a9876c74926a58
Grebenkina <grebenkina.2490@gmail.com>
2022-11-23 14:17:49 +0800

    try to fix some test-problem

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,13 @@ export class App extends React.Component<{}, State> {
   };
 
   addOne = () => {
-    this.setState((state) => ({
+    this.setState(state => ({
       count: state.count + 1,
     }));
   };
 
   add100 = () => {
-    this.setState((state) => ({
+    this.setState(state => ({
       count: state.count + 100,
     }));
   };

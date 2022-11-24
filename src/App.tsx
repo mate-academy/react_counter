a3795f713231ccbf29be9b55b52591e2b81c4e4c
Andriy Shymkiv <winamondezik@gmail.com>
2022-11-24 16:18:52 +0200

    counter with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,40 @@
-import React from 'react';
 import './App.scss';
+import { useState } from 'react';
 
-type State = {
-  count: number,
+export const App: React.FC = () => {
+  const [counter, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="App__title">
+        {`Count: ${counter}`}
+      </h1>
+
+      <button
+        type="button"
+        className="App__add-one"
+        onClick={() => setCount(current => current + 1)}
+      >
+        Add 1
+      </button>
+
+      <button
+        type="button"
+        className="App__add-100"
+        onClick={() => setCount(current => current + 100)}
+      >
+        Add 100
+      </button>
+
+      <button
+        type="button"
+        className="App__increase"
+        onClick={counter % 5 === 0
+          ? () => setCount(current => current + 100)
+          : () => setCount(current => current + 1)}
+      >
+        Increase
+      </button>
+    </div>
+  );
 };
-
-export class App extends React.Component<{}, State> {
-  state: Readonly<State> = {
-    count: 0,
-  };
-
-  addOne = () => {
-    this.setState(prevState => ({
-      count: prevState.count + 1,
-    }));
-  };
-
-  add100 = () => {
-    this.setState(prevState => ({
-      count: prevState.count + 100,
-    }));
-  };
-
-  increase = () => {
-    this.addOne();
-
-    if (this.state.count % 5 === 0) {
-      this.add100();
-    }
-  };
-
-  render() {
-    const { count } = this.state;
-
-    return (
-      <div className="App">
-        <h1 className="App__title">
-          {`Count: ${count}`}
-        </h1>
-
-        <button type="button" className="App__add-one" onClick={this.addOne}>
-          Add 1
-        </button>
-
-        <button type="button" className="App__add-100" onClick={this.add100}>
-          Add 100
-        </button>
-
-        <button type="button" className="App__increase" onClick={this.increase}>
-          Increase
-        </button>
-      </div>
-    );
-  }
-}

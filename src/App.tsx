1d991f2ecd53a416ab6dbc0d7d72336b770ecc99
OLEXANDRA KOROL <sandrakorol3@gmail.com>
2023-01-04 17:12:38 +0200

    styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import 'bulma/css/bulma.css';
 
 type State = {
   count: number,
@@ -35,20 +36,44 @@ export class App extends React.Component<{}, State> {
     const { count } = this.state;
 
     return (
-      <div className="App">
-        <h1 className="App__title">
+      <div className="App box">
+        <h1 className="title App__title">
           {`Count: ${count}`}
         </h1>
 
-        <button type="button" className="App__add-one" onClick={this.addOne}>
+        <button
+          type="button"
+          className="
+            button
+            is-info
+            addGap
+            App__add-one"
+          onClick={this.addOne}
+        >
           Add 1
         </button>
 
-        <button type="button" className="App__add-100" onClick={this.add100}>
+        <button
+          type="button"
+          className="
+            button
+            is-info
+            addGap
+            App__add-100"
+          onClick={this.add100}
+        >
           Add 100
         </button>
 
-        <button type="button" className="App__increase" onClick={this.increase}>
+        <button
+          type="button"
+          className="
+            button
+            is-info
+            addGap
+            App__increase"
+          onClick={this.increase}
+        >
           Increase
         </button>
       </div>

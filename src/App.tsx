fcd21546c25fa3dc29c16886e9ffbefda009fbe5
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2023-01-04 16:48:30 +0200

    Added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.scss';
+import {
+  Button, ButtonGroup, Box, Card, CardContent,
+  Typography, CardActions,
+} from '@mui/material';
 
 type State = {
   count: number,
@@ -26,7 +30,6 @@ export class App extends React.Component<{}, State> {
     });
   };
 
-  // DON'T change the code below
   increase = () => {
     this.addOne();
 
@@ -39,23 +42,31 @@ export class App extends React.Component<{}, State> {
     const { count } = this.state;
 
     return (
-      <div className="App">
-        <h1 className="App__title">
-          {`Count: ${count}`}
-        </h1>
-
-        <button type="button" className="App__add-one" onClick={this.addOne}>
-          Add 1
-        </button>
-
-        <button type="button" className="App__add-100" onClick={this.add100}>
-          Add 100
-        </button>
-
-        <button type="button" className="App__increase" onClick={this.increase}>
-          Increase
-        </button>
-      </div>
+      <Box
+        sx={{
+          width: 300,
+        }}
+      >
+        <Card variant="outlined">
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+              {`Count: ${count}`}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <ButtonGroup
+              variant="contained"
+              size="small"
+              color="success"
+              aria-label="alignment button group"
+            >
+              <Button onClick={this.addOne}>Add 1</Button>
+              <Button onClick={this.add100}>Add 100</Button>
+              <Button onClick={this.increase}>Increase</Button>
+            </ButtonGroup>
+          </CardActions>
+        </Card>
+      </Box>
     );
   }
 }

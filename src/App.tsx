a059ec60d08e6d28a7a27c98f3f8a50622f6c3d6
VladimirEdelmann <VladimirEdelmann@gmail.com>
2023-01-09 15:24:41 +0200

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+
+import sniperRifle from './img/sniper-rifle.png';
+import himars from './img/himars-icon.jpg';
+import battleGoose from './img/battle-goose.jpg';
+
+import 'bulma/css/bulma.min.css';
 import './App.scss';
 
 type State = {
@@ -35,22 +41,55 @@ export class App extends React.Component<{}, State> {
     const { count } = this.state;
 
     return (
-      <div className="App">
-        <h1 className="App__title">
+      <div className="App container is-widescreen mt-6 has-text-centered">
+        <p className="is-size-4">Good russians</p>
+        <h1 className="App__title is-size-5">
           {`Count: ${count}`}
         </h1>
 
-        <button type="button" className="App__add-one" onClick={this.addOne}>
-          Add 1
-        </button>
+        <div className="container my-4">
+          <button
+            type="button"
+            className="App__add-one button is-size-4 is-uppercase"
+            onClick={this.addOne}
+          >
+            <figure className="image is-48x48">
+              <img
+                src={sniperRifle}
+                alt="sniper rifle"
+              />
+            </figure>
+            Add 1
+          </button>
 
-        <button type="button" className="App__add-100" onClick={this.add100}>
-          Add 100
-        </button>
+          <button
+            type="button"
+            className="App__add-100 button mx-2 is-size-4 is-uppercase"
+            onClick={this.add100}
+          >
+            <figure className="image is-48x48 mt-4 mr-1">
+              <img
+                src={himars}
+                alt="HIMARS"
+              />
+            </figure>
+            Add 100
+          </button>
 
-        <button type="button" className="App__increase" onClick={this.increase}>
-          Increase
-        </button>
+          <button
+            type="button"
+            className="App__increase button is-size-4 is-uppercase"
+            onClick={this.increase}
+          >
+            <figure className="image is-48x48 mt-3 mr-1">
+              <img
+                src={battleGoose}
+                alt="Battle goose"
+              />
+            </figure>
+            Increase
+          </button>
+        </div>
       </div>
     );
   }

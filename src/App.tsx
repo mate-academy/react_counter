9f99f6978fab7703c877a3b7a7a4a01a50482316
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-03-16 19:04:08 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ export class App extends React.Component<{}, State> {
   addOne = () => {
     this.setState((state) => ({
       count: state.count + 1,
-    }))
+    }));
   };
 
   add100 = () => {
     this.setState((state) => ({
       count: state.count + 100,
-    }))
+    }));
   };
 
   // DON'T change the code below

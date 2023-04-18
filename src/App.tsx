24128c50de418d64bba67749ab0867d21951cbd1
Mark Boiko <boikomark555@gmail.com>
2023-04-18 18:32:34 +0300

    improve code

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ export class App extends React.Component<{}, State> {
   };
 
   addOne = () => {
-    this.setState(state => ({ count: state.count + 1 }));
+    this.setState(({ count }) => ({ count: count + 1 }));
   };
 
   add100 = () => {
-    this.setState(state => ({ count: state.count + 100 }));
+    this.setState(({ count }) => ({ count: count + 100 }));
   };
 
   // DON'T change the code below

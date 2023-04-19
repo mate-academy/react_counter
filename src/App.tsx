8a7522ba2f3a60b4eb9f7ee58b62f056d56cba83
Valery Sazonov <vksazonov@gmail.com>
2023-04-19 14:54:21 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,13 @@ export class App extends React.Component<{}, State> {
   };
 
   addOne = () => {
-    // write code here
+    this.setState(prevState => ({ count: prevState.count + 1 }));
   };
 
   add100 = () => {
-    // write code here
+    this.setState(prevState => ({ count: prevState.count + 100 }));
   };
 
-  // DON'T change the code below
   increase = () => {
     this.addOne();
 

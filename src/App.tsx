4a44ab24954105071f6ca069dfb00e1b573b4923
Vania Yarovyi <yarovyi@gmail.com>
2023-06-13 16:54:29 +0300

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Typography,
+} from '@mui/material';
 import './App.scss';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 type State = {
   count: number,
@@ -35,23 +46,79 @@ export class App extends React.Component<{}, State> {
     const { count } = this.state;
 
     return (
-      <div className="App">
-        <h1 className="App__title">
-          {`Count: ${count}`}
-        </h1>
+      <Container
+        maxWidth="sm"
+      >
+        <Box
+          sx={{
+            border: 3,
+            borderColor: 'success.main',
+            borderRadius: 25,
+            display: 'flex',
+            justifyContent: 'center',
+            alignContent: 'center',
+          }}
+        >
+          <div className="App">
+            <h1 className="App__title">
+              <Typography
+                variant="h4"
+                align="center"
+                color="success.main"
+              >
+                {`Count: ${count}`}
+              </Typography>
+            </h1>
 
-        <button type="button" className="App__add-one" onClick={this.addOne}>
-          Add 1
-        </button>
+            <ButtonGroup
+              variant="outlined"
+              color="success"
+            >
+              <Button>
+                <button
+                  type="button"
+                  className="App__add-one"
+                  onClick={this.addOne}
+                >
+                  <Typography
+                    color="success.main"
+                  >
+                    Add 1
+                  </Typography>
+                </button>
+              </Button>
 
-        <button type="button" className="App__add-100" onClick={this.add100}>
-          Add 100
-        </button>
+              <Button>
+                <button
+                  type="button"
+                  className="App__add-100"
+                  onClick={this.add100}
+                >
+                  <Typography
+                    color="success.main"
+                  >
+                    Add 100
+                  </Typography>
+                </button>
+              </Button>
 
-        <button type="button" className="App__increase" onClick={this.increase}>
-          Increase
-        </button>
-      </div>
+              <Button>
+                <button
+                  type="button"
+                  className="App__increase"
+                  onClick={this.increase}
+                >
+                  <Typography
+                    color="success.main"
+                  >
+                    Increase
+                  </Typography>
+                </button>
+              </Button>
+            </ButtonGroup>
+          </div>
+        </Box>
+      </Container>
     );
   }
 }
